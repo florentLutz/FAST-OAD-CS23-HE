f2d9e4ab52d04c23f445e23d97830971975df9a5
Florent <66668699+florentLutz@users.noreply.github.com>
2022-06-07 14:21:02 +0200

    Folder organisation

---
+++ src/fastga_he/models/propulsion/components/propulsion_api.py
@@ -0,0 +1,4 @@
+"""This module gathers key FAST-OAD-CS23-HE classes and functions for convenient import."""
+# This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
+# Electric Aircraft.
+# Copyright (C) 2022 ISAE-SUPAERO
