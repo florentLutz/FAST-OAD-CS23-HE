8807f6dd4eead0bd77e838e3e5c1806f48a68d59
vhabrard <112182664+vhabrard@users.noreply.github.com>
2022-11-24 16:45:17 +0100

    Create hydrogen tank
    hydrogen tank mass and volume

--- src/fastga_he/models/thermal/components/hydrogen_tank.py
+++ src/fastga_he/models/thermal/components/hydrogen_tank.py
@@ -4,10 +4,14 @@ import openmdao.api as om
 
 class ComputeHydrogenTank(om.ExplicitComponent):
     
+    # Volume model from assumptions in: 
+    # Palladino, V., Jordan, A., Bartoli, N., Schmollgruber, P., Pommier-Budinger, V., and Benard, E., 
+    # “Preliminary studies of aregional aircraft with hydrogen-based hybrid propulsion,” AIAA Aviation, 2021
+    
     def setup(self):
 
         self.add_input(name="H2_mass_flow", units = "kg/s", val=np.nan)
-        self.add_input(name="H2_gravimetric_efficiency", val=0.5)
+        self.add_input(name="H2_gravimetric_efficiency", val=np.nan)
         self.add_input(name="H2_volumetric_efficiency", val=np.nan)
         self.add_input(name="time", units="s", val=np.nan)
 
@@ -21,7 +25,7 @@ class ComputeHydrogenTank(om.ExplicitComponent):
         n_v = inputs["H2_volumetric_efficiency"]
 
         M_H2_tank = M_H2/n_g - M_H2
-        V_H2_tank = 0.0366*M_H2 + 0.1215
+        V_H2_tank = 0.0366*M_H2 + 0.1215 
 
         outputs["H2_tank_mass"] = M_H2_tank
         outputs["H2_tank_volume"] = V_H2_tank
