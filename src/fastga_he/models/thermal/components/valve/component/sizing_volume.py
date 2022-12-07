e3596b1bbdb9da89f4ec33cf5435e9ca443ce18d
Valentine HABRARD <v.habrard@ad.isae-supaero.fr>
2022-12-07 10:15:04 +0100

    adding valve model

--- src/fastga_he/models/thermal/components/valve/component/sizing_volume.py
+++ src/fastga_he/models/thermal/components/valve/component/sizing_volume.py
@@ -3,30 +3,21 @@ import scipy.constants as sc
 import openmdao.api as om
 
 
-class ComputePipeVolume(om.ExplicitComponent):
+class ComputeValvzVolume(om.ExplicitComponent):
     def setup(self):
 
         self.add_input(
-            name="data:thermal:pipes:distance",
+            name="data:thermal:coolant:mass:flow",
             units="m",
             val=np.nan,
-            desc="total distance of pipes in TMS",
+            desc="coolant mass flow",
         )
-
-        self.add_input(
-            name="data:thermal:pipes:radius",
-            units="m",
-            val=np.nan,
-            desc="pipe radiuss",
-        )
-
-        self.add_output(name="data:thermal:pipes:coolant:volume", units="m**3")
 
     def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
 
-        d_pipe = inputs["data:thermal:pipes:distance"]
-        R_pipe = inputs["data:thermal:pipes:radius"]
+        m_flow = inputs["data:thermal:coolant:mass_flow"]
 
-        V_coolant_pipe = np.pi * d_pipe * R_pipe**2
+        if m_flow
+        V_valve = 0.0003*m_flow-0.0001 
 
-        outputs["data:thermal:pipes:coolant:volume"] = V_coolant_pipe
+        outputs["data:thermal:valvevolume"] = V_valve
