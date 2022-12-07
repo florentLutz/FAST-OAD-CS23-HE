b955dab3fe5d35589fc52f9c049f8894de35d2bf
Florent <66668699+florentLutz@users.noreply.github.com>
2022-12-07 10:33:17 +0100

    Architecture for mission vector revamp
    There is an issue with the mission vecotr which lies in the fact that the option to give the power train file path is not there, so we will need to slightly adjust most of the default mission vector to accomodate for it --> Need for a generic method but with submodels it might be complex !!!
    Also highlighted the fact that the interfacing is not fully donne yet, the consumption part is not ready !

---
+++ src/fastga_he/models/performances/units_tests/test_mission_vector.py
@@ -0,0 +1,43 @@
+"""
+Test mission vector module.
+"""
+#  This file is part of FAST-OAD_CS23 : A framework for rapid Overall Aircraft Design
+#  Copyright (C) 2022  ONERA & ISAE-SUPAERO
+#  FAST is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import pytest
+import numpy as np
+
+from fastga_he.models.performances.mission_vector.mission_vector import MissionVector
+
+from tests.testing_utilities import run_system, get_indep_var_comp, list_inputs
+
+XML_FILE = "sample_ac.xml"
+
+
+def test_mission_vector():
+
+    # Research independent input value in .xml file
+    ivc = get_indep_var_comp(
+        list_inputs(MissionVector(propulsion_id=ENGINE_WRAPPER)),
+        __file__,
+        XML_FILE,
+    )
+
+    problem = run_system(
+        MissionVector(propulsion_id=ENGINE_WRAPPER),
+        ivc,
+    )
+    sizing_fuel = problem.get_val("data:mission:sizing:fuel", units="kg")
+    assert sizing_fuel == pytest.approx(208.09, abs=1e-2)
+    sizing_energy = problem.get_val("data:mission:sizing:energy", units="kW*h")
+    assert sizing_energy == pytest.approx(0.0, abs=1e-2)
