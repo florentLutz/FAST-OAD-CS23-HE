9d01715e6d266083afbc68942355c07e330814fb
Florent <66668699+florentLutz@users.noreply.github.com>
2023-01-18 08:12:00 +0100

    Second axis on performances viewer
    It is now possible to display a second set of data in the performances viewer at the same time as the first one. Color scheme are a bit different and a button was added to enforce the same axis so that efficiencies could be compared for instance.

--- src/fastga_he/gui/performances_viewer.py
+++ src/fastga_he/gui/performances_viewer.py
@@ -9,13 +9,21 @@ import plotly.graph_objects as go
 import plotly.express as px
 from IPython.display import clear_output, display
 
+from plotly.subplots import make_subplots
+
 from fastga_he.powertrain_builder.powertrain import PROMOTION_FROM_MISSION
 
 COLOR_DICTIONARY = {
-    "sizing:main_route:climb": px.colors.qualitative.Prism[3],
-    "sizing:main_route:cruise": px.colors.qualitative.Prism[4],
-    "sizing:main_route:descent": px.colors.qualitative.Prism[5],
-    "sizing:main_route:reserve": px.colors.qualitative.Prism[6],
+    "sizing:main_route:climb": px.colors.qualitative.Prism[1],
+    "sizing:main_route:cruise": px.colors.qualitative.Prism[2],
+    "sizing:main_route:descent": px.colors.qualitative.Prism[3],
+    "sizing:main_route:reserve": px.colors.qualitative.Prism[4],
+}
+COLOR_DICTIONARY_AXIS_2 = {
+    "sizing:main_route:climb": px.colors.qualitative.Prism[5],
+    "sizing:main_route:cruise": px.colors.qualitative.Prism[6],
+    "sizing:main_route:descent": px.colors.qualitative.Prism[7],
+    "sizing:main_route:reserve": px.colors.qualitative.Prism[8],
 }
 
 
@@ -84,6 +92,12 @@ class PerformancesViewer:
         # The y selector
         self._y_widget = None
 
+        # The y2 selector
+        self._y2_widget = None
+
+        # The button to ensure same axis
+        self._axis_ensurer = None
+
         self.plot_height = plot_height
         self.plot_width = plot_width
 
@@ -109,8 +123,12 @@ class PerformancesViewer:
 
                 x_name = self._x_widget.value
                 y_name = self._y_widget.value
+                y2_name = self._y2_widget.value
 
-                fig = go.Figure()
+                if y2_name == "None":
+                    fig = go.Figure()
+                else:
+                    fig = make_subplots(specs=[[{"secondary_y": True}]])
 
                 # If both mission and pt data are present, we change the color according to the
                 # phase to make it more readable
@@ -127,8 +145,27 @@ class PerformancesViewer:
                             mode="markers",
                             marker={"color": COLOR_DICTIONARY[name]},
                             name=name,
+                            legendgroup="Primary axis",
+                            legendgrouptitle_text="primary_axis",
                         )
-                        fig.add_trace(scatter)
+
+                        if y2_name != "None":
+                            y_2 = self.data.loc[self.data["name"] == name, y2_name]
+                            scatter_2 = go.Scatter(
+                                x=x,
+                                y=y_2,
+                                mode="markers",
+                                marker={"color": COLOR_DICTIONARY_AXIS_2[name]},
+                                name=name,
+                                legendgroup="Secondary axis",
+                                legendgrouptitle_text="secondary_axis",
+                            )
+
+                            fig.add_trace(scatter, secondary_y=False)
+                            fig.add_trace(scatter_2, secondary_y=True)
+
+                        else:
+                            fig.add_trace(scatter)
                 else:
 
                     # pylint: disable=invalid-name # that's a common naming
@@ -136,23 +173,62 @@ class PerformancesViewer:
                     # pylint: disable=invalid-name # that's a common naming
                     y = self.data[y_name]
 
-                    scatter = go.Scatter(x=x, y=y, mode="markers")
-                    fig.add_trace(scatter)
+                    scatter = go.Scatter(
+                        x=x,
+                        y=y,
+                        mode="markers",
+                        legendgroup="Primary axis",
+                        legendgrouptitle_text="primary_axis",
+                    )
 
-                fig.update_layout(
-                    title_text="Power train performances on the mission",
-                    title_x=0.5,
-                    xaxis_title=x_name,
-                    yaxis_title=y_name,
-                    showlegend=True,
-                )
+                    if y2_name != "None":
+                        y_2 = self.data[y2_name]
+                        scatter_2 = go.Scatter(
+                            x=x,
+                            y=y_2,
+                            mode="markers",
+                            legendgroup="Secondary axis",
+                            legendgrouptitle_text="secondary_axis",
+                        )
+
+                        fig.add_trace(scatter, secondary_y=False)
+                        fig.add_trace(scatter_2, secondary_y=True)
+                    else:
+                        fig.add_trace(scatter)
+
+                if y2_name != "None":
+                    fig.update_layout(
+                        title_text="Power train performances on the mission",
+                        title_x=0.5,
+                        showlegend=True,
+                    )
+                    fig.update_xaxes(title_text=x_name)
+                    fig.update_yaxes(title_text=y_name, secondary_y=False)
+                    fig.update_yaxes(title_text=y2_name, secondary_y=True)
+
+                    if self._axis_ensurer.value:
+                        y_min = min(y.min(), y_2.min())
+                        y_max = max(y.max(), y_2.max())
+
+                        fig.update_yaxes(range=[0.95 * y_min, 1.05 * y_max], secondary_y=False)
+                        fig.update_yaxes(range=[0.95 * y_min, 1.05 * y_max], secondary_y=True)
+
+                else:
+                    fig.update_layout(
+                        title_text="Power train performances on the mission",
+                        title_x=0.5,
+                        xaxis_title=x_name,
+                        yaxis_title=y_name,
+                        showlegend=True,
+                    )
                 if self.plot_height:
                     fig.update_layout(height=self.plot_height)
 
                 if self.plot_width:
                     fig.update_layout(width=self.plot_width)
 
-                fig.show()
+                fig = go.FigureWidget(fig)
+                display(fig)
 
         # Check if time is in column name to put it as the x axis by default
         if "time" in keys:
@@ -160,15 +236,38 @@ class PerformancesViewer:
         else:
             index_x = 2
 
-        # By default ground distance
         self._x_widget = widgets.Dropdown(value=keys[index_x], options=keys)
         self._x_widget.observe(show_plots, "value")
-        # By default altitude
+
         self._y_widget = widgets.Dropdown(value=keys[1], options=keys)
         self._y_widget.observe(show_plots, "value")
 
+        self._y2_widget = widgets.Dropdown(value="None", options=list(keys) + ["None"])
+        self._y2_widget.observe(show_plots, "value")
+
+        self._axis_ensurer = widgets.ToggleButton(
+            value=False,
+            description="Ensure same axis",
+            disabled=False,
+            button_style="success",
+            tooltip="Check me to ensure that the primary and secondary axis will have the same y "
+            "range, useful for comparing efficiencies for instance",
+            icon="check",
+        )
+        self._axis_ensurer.observe(show_plots, "value")
+
+        show_plots()
+
         toolbar = widgets.HBox(
-            [widgets.Label(value="x:"), self._x_widget, widgets.Label(value="y:"), self._y_widget]
+            [
+                widgets.Label(value="x:"),
+                self._x_widget,
+                widgets.Label(value="y:"),
+                self._y_widget,
+                widgets.Label(value="y2:"),
+                self._y2_widget,
+                self._axis_ensurer,
+            ]
         )
 
         display(toolbar, output)
