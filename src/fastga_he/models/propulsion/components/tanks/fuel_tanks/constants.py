c6a51b9f4a930d6d08f00895ddc8936cb342d150
Florent <66668699+florentLutz@users.noreply.github.com>
2023-09-25 15:05:35 +0200

    Tank component setup
    Created all the right folders :tophat:

---
+++ src/fastga_he/models/propulsion/components/tanks/fuel_tanks/constants.py
@@ -0,0 +1,7 @@
+# This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
+# Electric Aircraft.
+# Copyright (C) 2022 ISAE-SUPAERO
+
+SUBMODEL_CONSTRAINTS_FUEL_TANK_CAPACITY = "submodel.propulsion.constraints.fuel_tank.capacity"
+
+POSSIBLE_POSITION = ["in_the_wing", "wing_pod", "in_the_fuselage"]
