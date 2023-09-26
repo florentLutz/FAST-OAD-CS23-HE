f86b924a20b74af531806df74b5f12031dd013e5
Florent <66668699+florentLutz@users.noreply.github.com>
2023-09-26 15:25:21 +0200

    Fuel tank slipstream
    Does not do much expect output zero as expected. Next commit will be about plugging the tanks inside the pt builder like a generic component. Then we will add the assembler that properly compute the variation of CG and the "distributed" fuel loads (which will bring some slight change to the sizing component because we will need the y_ratios even for the pod because of how i was previously implemented)

--- src/fastga_he/models/propulsion/components/tanks/fuel_tanks/__init__.py
+++ src/fastga_he/models/propulsion/components/tanks/fuel_tanks/__init__.py
@@ -1,3 +1,7 @@
 # This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
 # Electric Aircraft.
 # Copyright (C) 2022 ISAE-SUPAERO
+
+from .components.sizing_tank import SizingFuelTank
+from .components.perf_fuel_tank import PerformancesFuelTank
+from .components.slipstream_fuel_tank import SlipstreamFuelTank
