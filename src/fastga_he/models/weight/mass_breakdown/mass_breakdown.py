8890a62b45a2d1450de54924b5ba66ddbfe17f50
Florent <66668699+florentLutz@users.noreply.github.com>
2024-04-29 09:03:36 +0200

    OWE  ok
    OWE is more or less OK, 30kg above but that represents less than 5 percent

---
+++ src/fastga_he/models/weight/mass_breakdown/mass_breakdown.py
@@ -0,0 +1,80 @@
+"""
+Main components for mass breakdown.
+"""
+#  This file is part of FAST-OAD_CS23 : A framework for rapid Overall Aircraft Design
+#  Copyright (C) 2022  ONERA & ISAE-SUPAERO
+#  FAST is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import openmdao.api as om
+
+import fastoad.api as oad
+
+from fastga.models.weight.mass_breakdown.constants import SUBMODEL_OWE, SUBMODEL_PAYLOAD_MASS
+from fastga.models.weight.constants import SUBMODEL_MASS_BREAKDOWN
+
+from .constants import SUBMODEL_MZFW_MLW
+
+from fastga.models.options import PAYLOAD_FROM_NPAX
+
+
+@oad.RegisterSubmodel(SUBMODEL_MASS_BREAKDOWN, "fastga_he.submodel.weight.mass_breakdown.modular")
+class MassBreakdown(om.Group):
+    """
+    Computes analytically the mass of each part of the aircraft, and the resulting sum,
+    the Overall Weight Empty (OWE).
+
+    Some models depend on MZFW (Max Zero Fuel Weight) and MTOW (Max TakeOff Weight),
+    which depend on OWE.
+
+    This model cycles for having consistent OWE, MZFW and MTOW based on MFW.
+
+    Options:
+    - payload_from_npax: If True (default), payload masses will be computed from NPAX, if False
+                         design payload mass and maximum payload mass must be provided.
+    """
+
+    def __init__(self, **kwargs):
+        """Defining solvers for mass breakdown computation resolution."""
+        super().__init__(**kwargs)
+
+        # Solvers setup
+        self.nonlinear_solver = om.NonlinearBlockGS()
+        self.nonlinear_solver.options["debug_print"] = True
+        self.nonlinear_solver.options["err_on_non_converge"] = True
+        self.nonlinear_solver.options["iprint"] = 0
+        self.nonlinear_solver.options["maxiter"] = 50
+
+        self.linear_solver = om.LinearBlockGS()
+        self.linear_solver.options["err_on_non_converge"] = True
+        self.linear_solver.options["iprint"] = 0
+        self.linear_solver.options["maxiter"] = 10
+
+    def initialize(self):
+        self.options.declare(PAYLOAD_FROM_NPAX, types=bool, default=True)
+        self.options.declare("propulsion_id", default="", types=str)
+
+    def setup(self):
+        if self.options[PAYLOAD_FROM_NPAX]:
+            self.add_subsystem(
+                "payload", oad.RegisterSubmodel.get_submodel(SUBMODEL_PAYLOAD_MASS), promotes=["*"]
+            )
+        propulsion_option = {"propulsion_id": self.options["propulsion_id"]}
+        self.add_subsystem(
+            "owe",
+            oad.RegisterSubmodel.get_submodel(SUBMODEL_OWE, options=propulsion_option),
+            promotes=["*"],
+        )
+        self.add_subsystem(
+            "update_mzfw_and_mlw",
+            oad.RegisterSubmodel.get_submodel(SUBMODEL_MZFW_MLW),
+            promotes=["*"],
+        )
---
+++ src/fastga_he/models/weight/mass_breakdown/update_mlw_and_mzfw.py
@@ -0,0 +1,91 @@
+"""
+Main component for mass breakdown.
+"""
+#  This file is part of FAST-OAD_CS23 : A framework for rapid Overall Aircraft Design
+#  Copyright (C) 2022  ONERA & ISAE-SUPAERO
+#  FAST is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import numpy as np
+
+import openmdao.api as om
+import fastoad.api as oad
+
+from .constants import SUBMODEL_MZFW_MLW
+
+
+@oad.RegisterSubmodel(SUBMODEL_MZFW_MLW, "fastga_he.submodel.weight.mass.mzfw_and_mlw.legacy")
+class UpdateMLWandMZFW(om.ExplicitComponent):
+    """
+    Computes Maximum Landing Weight and Maximum Zero Fuel Weight from
+    Overall Empty Weight and Maximum Payload.
+    """
+
+    def setup(self):
+        self.add_input("data:weight:aircraft:OWE", val=np.nan, units="kg")
+        self.add_input("data:weight:aircraft:max_payload", val=np.nan, units="kg")
+        self.add_input("data:weight:aircraft:payload", val=np.nan, units="kg")
+        self.add_input("settings:weight:aircraft:MLW_MZFW_ratio", val=1.06)
+
+        self.add_output("data:weight:aircraft:MZFW", units="kg")
+        self.declare_partials("data:weight:aircraft:MZFW", "data:weight:aircraft:OWE", val=1.0)
+        self.declare_partials(
+            "data:weight:aircraft:MZFW", "data:weight:aircraft:max_payload", val=1.0
+        )
+
+        self.add_output("data:weight:aircraft:ZFW", units="kg")
+        self.declare_partials("data:weight:aircraft:ZFW", "data:weight:aircraft:OWE", val=1.0)
+        self.declare_partials("data:weight:aircraft:ZFW", "data:weight:aircraft:payload", val=1.0)
+
+        self.add_output("data:weight:aircraft:MLW", units="kg")
+        self.declare_partials(
+            "data:weight:aircraft:MLW",
+            [
+                "data:weight:aircraft:OWE",
+                "data:weight:aircraft:max_payload",
+                "settings:weight:aircraft:MLW_MZFW_ratio",
+            ],
+            method="exact",
+        )
+
+    def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
+        owe = inputs["data:weight:aircraft:OWE"]
+        max_pl = inputs["data:weight:aircraft:max_payload"]
+        pl = inputs["data:weight:aircraft:payload"]
+
+        mzfw = owe + max_pl
+        zfw = owe + pl
+
+        mlw = inputs["settings:weight:aircraft:MLW_MZFW_ratio"] * mzfw
+
+        outputs["data:weight:aircraft:MZFW"] = mzfw
+        outputs["data:weight:aircraft:ZFW"] = zfw
+        outputs["data:weight:aircraft:MLW"] = mlw
+
+    def compute_partials(self, inputs, partials, discrete_inputs=None):
+
+        owe = inputs["data:weight:aircraft:OWE"]
+        max_pl = inputs["data:weight:aircraft:max_payload"]
+
+        mzfw = owe + max_pl
+
+        partials[
+            "data:weight:aircraft:MLW",
+            "data:weight:aircraft:OWE",
+        ] = inputs["settings:weight:aircraft:MLW_MZFW_ratio"]
+        partials[
+            "data:weight:aircraft:MLW",
+            "data:weight:aircraft:max_payload",
+        ] = inputs["settings:weight:aircraft:MLW_MZFW_ratio"]
+        partials[
+            "data:weight:aircraft:MLW",
+            "settings:weight:aircraft:MLW_MZFW_ratio",
+        ] = mzfw
