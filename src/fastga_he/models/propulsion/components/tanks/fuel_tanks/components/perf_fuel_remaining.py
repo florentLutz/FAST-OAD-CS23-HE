6651640aaea1ba7f89faa45916225870019f2b4e
Florent <66668699+florentLutz@users.noreply.github.com>
2024-06-06 09:30:17 +0200

    Fuel Tank done

--- src/fastga_he/models/propulsion/components/tanks/fuel_tanks/components/perf_fuel_remaining.py
+++ src/fastga_he/models/propulsion/components/tanks/fuel_tanks/components/perf_fuel_remaining.py
@@ -48,7 +48,29 @@ class PerformancesFuelRemainingMission(om.ExplicitComponent):
             desc="Fuel remaining inside the tank at each time step",
         )
 
-        self.declare_partials(of="*", wrt="*", method="exact")
+        self.declare_partials(
+            of="fuel_remaining_t",
+            wrt="data:propulsion:he_power_train:fuel_tank:"
+            + fuel_tank_id
+            + ":fuel_consumed_mission",
+            method="exact",
+            rows=np.arange(number_of_points),
+            cols=np.zeros(number_of_points),
+            val=np.ones(number_of_points),
+        )
+
+        partials_fuel_consumed = -(
+            np.tri(number_of_points, number_of_points) - np.eye(number_of_points)
+        )
+
+        self.declare_partials(
+            of="fuel_remaining_t",
+            wrt="fuel_consumed_t",
+            method="exact",
+            rows=np.where(partials_fuel_consumed != 0)[0],
+            cols=np.where(partials_fuel_consumed != 0)[1],
+            val=-np.ones(len(np.where(partials_fuel_consumed != 0)[0])),
+        )
 
     def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
 
@@ -61,17 +83,4 @@ class PerformancesFuelRemainingMission(om.ExplicitComponent):
 
         outputs["fuel_remaining_t"] = np.full(number_of_points, total_fuel) - np.cumsum(
             np.concatenate((np.zeros(1), inputs["fuel_consumed_t"][:-1]))
-        )
-
-    def compute_partials(self, inputs, partials, discrete_inputs=None):
-
-        fuel_tank_id = self.options["fuel_tank_id"]
-        number_of_points = self.options["number_of_points"]
-
-        partials[
-            "fuel_remaining_t",
-            "data:propulsion:he_power_train:fuel_tank:" + fuel_tank_id + ":fuel_consumed_mission",
-        ] = np.ones(number_of_points)
-        partials["fuel_remaining_t", "fuel_consumed_t"] = -(
-            np.tri(number_of_points, number_of_points) - np.eye(number_of_points)
         )
