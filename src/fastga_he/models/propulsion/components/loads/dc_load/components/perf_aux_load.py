242f4132d3f29c6e5bd12d13aa8c503497ea0d54
Florent <66668699+florentLutz@users.noreply.github.com>
2024-06-26 10:22:41 +0200

    Initial setup and performances
    Created the right folder and added components for the performances computation

---
+++ src/fastga_he/models/propulsion/components/loads/dc_load/components/perf_aux_load.py
@@ -0,0 +1,45 @@
+# This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
+# Electric Aircraft.
+# Copyright (C) 2022 ISAE-SUPAERO
+
+import openmdao.api as om
+import numpy as np
+
+from .perf_power_in import PerformancesPowerIn
+from .perf_current_in import PerformancesCurrentIn
+from .perf_maximum import PerformancesMaximum
+
+
+class PerformancesAuxLoad(om.Group):
+    def initialize(self):
+
+        self.options.declare(
+            "number_of_points", default=1, desc="number of equilibrium to be treated"
+        )
+        self.options.declare(
+            name="aux_load_id",
+            default=None,
+            desc="Identifier of the auxiliary load",
+            allow_none=False,
+        )
+
+    def setup(self):
+
+        aux_load_id = self.options["aux_load_id"]
+        number_of_points = self.options["number_of_points"]
+
+        self.add_subsystem(
+            "power_in_mission",
+            PerformancesPowerIn(aux_load_id=aux_load_id, number_of_points=number_of_points),
+            promotes=["*"],
+        )
+        self.add_subsystem(
+            "current_in",
+            PerformancesCurrentIn(number_of_points=number_of_points),
+            promotes=["*"],
+        )
+        self.add_subsystem(
+            "maximum",
+            PerformancesMaximum(aux_load_id=aux_load_id, number_of_points=number_of_points),
+            promotes=["*"],
+        )
---
+++ src/fastga_he/models/propulsion/components/loads/dc_load/unit_tests/test_aux_load.py
@@ -0,0 +1,104 @@
+# This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
+# Electric Aircraft.
+# Copyright (C) 2022 ISAE-SUPAERO
+
+import openmdao.api as om
+import pytest
+import numpy as np
+
+from ..components.perf_power_in import PerformancesPowerIn
+from ..components.perf_current_in import PerformancesCurrentIn
+from ..components.perf_maximum import PerformancesMaximum
+
+from ..components.perf_aux_load import PerformancesAuxLoad
+
+from tests.testing_utilities import run_system, get_indep_var_comp
+
+XML_FILE = "ref_aux_load.xml"
+NB_POINTS_TEST = 10
+
+
+def test_power_in_mission():
+
+    ivc = om.IndepVarComp()
+    ivc.add_output(
+        "data:propulsion:he_power_train:aux_load:aux_load_1:power_in_mission",
+        val=15.0,
+        units="kW",
+    )
+
+    problem = run_system(
+        PerformancesPowerIn(aux_load_id="aux_load_1", number_of_points=NB_POINTS_TEST), ivc
+    )
+
+    assert problem.get_val("power_in", units="kW") == pytest.approx(
+        np.full(NB_POINTS_TEST, 15.0), rel=1e-2
+    )
+
+    problem.check_partials(compact_print=True)
+
+    ivc2 = om.IndepVarComp()
+    ivc2.add_output(
+        "data:propulsion:he_power_train:aux_load:aux_load_1:power_in_mission",
+        val=np.linspace(8.0, 12.0, NB_POINTS_TEST),
+        units="kW",
+    )
+
+    problem2 = run_system(
+        PerformancesPowerIn(aux_load_id="aux_load_1", number_of_points=NB_POINTS_TEST), ivc2
+    )
+
+    assert problem2.get_val("power_in", units="kW") == pytest.approx(
+        np.linspace(8.0, 12.0, NB_POINTS_TEST), rel=1e-2
+    )
+
+    problem2.check_partials(compact_print=True)
+
+
+def test_current_in():
+
+    ivc = om.IndepVarComp()
+    ivc.add_output("power_in", units="kW", val=np.linspace(8.0, 12.0, NB_POINTS_TEST))
+    ivc.add_output("dc_voltage_in", units="V", val=np.full(NB_POINTS_TEST, 400.0))
+
+    problem = run_system(PerformancesCurrentIn(number_of_points=NB_POINTS_TEST), ivc)
+
+    assert problem.get_val("dc_current_in", units="A") == pytest.approx(
+        np.linspace(20.0, 30.0, NB_POINTS_TEST), rel=1e-2
+    )
+
+    problem.check_partials(compact_print=True)
+
+
+def test_maximum():
+
+    ivc = om.IndepVarComp()
+    ivc.add_output("power_in", units="kW", val=np.linspace(8.0, 12.0, NB_POINTS_TEST))
+
+    problem = run_system(
+        PerformancesMaximum(aux_load_id="aux_load_1", number_of_points=NB_POINTS_TEST), ivc
+    )
+
+    assert problem.get_val(
+        "data:propulsion:he_power_train:aux_load:aux_load_1:power_max", units="kW"
+    ) == pytest.approx(12.0, rel=1e-2)
+
+    problem.check_partials(compact_print=True)
+
+
+def test_performances():
+
+    input_list = ["data:propulsion:he_power_train:aux_load:aux_load_1:power_in_mission"]
+    ivc = get_indep_var_comp(input_list, __file__, XML_FILE)
+    ivc.add_output("dc_voltage_in", units="V", val=np.full(NB_POINTS_TEST, 400.0))
+
+    # Run problem and check obtained value(s) is/(are) correct
+    problem = run_system(
+        PerformancesAuxLoad(aux_load_id="aux_load_1", number_of_points=NB_POINTS_TEST), ivc
+    )
+
+    assert problem.get_val(
+        "data:propulsion:he_power_train:aux_load:aux_load_1:power_max", units="kW"
+    ) == pytest.approx(10.0, rel=1e-2)
+
+    problem.check_partials(compact_print=True)
