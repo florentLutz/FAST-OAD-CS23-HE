c98d201370907e7ce15fb256c34b07b73cc2e98e
Florent <66668699+florentLutz@users.noreply.github.com>
2025-03-27 11:45:15 +0100

    Displacement volume computation
    Very similar to the original one with only a change in default values

--- src/fastga_he/models/propulsion/components/source/ice_rotax/unit_tests/test_ice_rotax.py
+++ src/fastga_he/models/propulsion/components/source/ice_rotax/unit_tests/test_ice_rotax.py
@@ -9,7 +9,27 @@ import openmdao.api as om
 
 from stdatm import Atmosphere
 
+from ..components.sizing_displacement_volume import SizingHighRPMICEDisplacementVolume
+
 from tests.testing_utilities import run_system, get_indep_var_comp, list_inputs
 
 XML_FILE = "sample_ice_rotax.xml"
 NB_POINTS_TEST = 10
+
+
+def test_displacement_volume():
+    inputs_list = [
+        "data:propulsion:he_power_train:high_rpm_ICE:ice_1:power_rating_SL",
+        "data:propulsion:he_power_train:high_rpm_ICE:ice_1:pme_max",
+    ]
+
+    ivc = get_indep_var_comp(inputs_list, __file__, XML_FILE)
+
+    # Run problem and check obtained value(s) is/(are) correct
+    problem = run_system(SizingHighRPMICEDisplacementVolume(high_rpm_ice_id="ice_1"), ivc)
+
+    assert problem.get_val(
+        "data:propulsion:he_power_train:high_rpm_ICE:ice_1:displacement_volume", units="cm**3"
+    ) == pytest.approx(1352.0, rel=1e-2)
+
+    problem.check_partials(compact_print=True)
