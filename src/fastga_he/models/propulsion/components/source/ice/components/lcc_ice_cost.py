ef3d010f0d708b6531349094ad7b0f52e611decf
johnathanasd <93048.87051.ko@gmail.com>
2025-04-01 11:06:01 +0200

    ICE cost

---
+++ src/fastga_he/models/propulsion/components/source/ice/components/lcc_ice_cost.py
@@ -0,0 +1,61 @@
+# This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
+# Electric Aircraft.
+# Copyright (C) 2025 ISAE-SUPAERO
+
+import numpy as np
+import openmdao.api as om
+
+
+class LCCICECost(om.ExplicitComponent):
+    def initialize(self):
+        self.options.declare(
+            name="ice_id",
+            default=None,
+            desc="Identifier of the Internal Combustion Engine",
+            allow_none=False,
+        )
+
+    def setup(self):
+        ice_id = self.options["ice_id"]
+
+        self.add_input(
+            "data:propulsion:he_power_train:ICE:" + ice_id + ":power_max_SL",
+            units="hp",
+            val=np.nan,
+            desc="Maximum power the motor has to provide at Sea Level",
+        )
+        self.add_input(
+            "data:cost:cpi_2012",
+            val=np.nan,
+            desc="Consumer price index relative to the year 2012",
+        )
+
+        self.add_output(
+            name="data:propulsion:he_power_train:ICE:" + ice_id + ":cost_per_engine",
+            units="USD",
+            val=1e4,
+            desc="Cost of the ICE per unit",
+        )
+
+        self.declare_partials(of="*", wrt="*", method="exact")
+
+    def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
+        ice_id = self.options["ice_id"]
+
+        outputs["data:propulsion:he_power_train:ICE:" + ice_id + ":cost_per_engine"] = (
+            174.0
+            * inputs["data:propulsion:he_power_train:ICE:" + ice_id + ":power_max_SL"]
+            * inputs["data:cost:cpi_2012"]
+        )
+
+    def compute_partials(self, inputs, partials, discrete_inputs=None):
+        ice_id = self.options["ice_id"]
+
+        partials[
+            "data:propulsion:he_power_train:ICE:" + ice_id + ":cost_per_engine",
+            "data:propulsion:he_power_train:ICE:" + ice_id + ":power_max_SL",
+        ] = 174.0 * inputs["data:cost:cpi_2012"]
+        partials[
+            "data:propulsion:he_power_train:ICE:" + ice_id + ":cost_per_engine",
+            "data:cost:cpi_2012",
+        ] = 174.0 * inputs["data:propulsion:he_power_train:ICE:" + ice_id + ":power_max_SL"]
--- src/fastga_he/models/propulsion/components/source/ice/unit_tests/test_ice.py
+++ src/fastga_he/models/propulsion/components/source/ice/unit_tests/test_ice.py
@@ -29,6 +29,8 @@ from ..components.sizing_ice_cg_y import SizingICECGY
 from ..components.pre_lca_prod_weight_per_fu import PreLCAICEProdWeightPerFU
 from ..components.pre_lca_use_emission_per_fu import PreLCAICEUseEmissionPerFU
 
+from ..components.lcc_ice_cost import LCCICECost
+
 from ..components.perf_torque import PerformancesTorque
 from ..components.perf_equivalent_sl_power import PerformancesEquivalentSeaLevelPower
 from ..components.perf_mean_effective_pressure import PerformancesMeanEffectivePressure
@@ -972,5 +974,27 @@ def test_emissions_per_fu():
     assert problem.get_val(
         "data:LCA:distribution:he_power_train:ICE:ice_1:lead_per_fu", units="kg"
     ) == pytest.approx(2.2911664e-07, rel=1e-3)
+
+    problem.check_partials(compact_print=True)
+
+
+def test_cost():
+    ivc = om.IndepVarComp()
+    ivc.add_output("data:cost:cpi_2012", val=1.4)
+    ivc.add_output(
+        "data:propulsion:he_power_train:ICE:ice_1:power_max_SL",
+        val=250.0,
+        units="kW",
+    )
+
+    # Run problem and check obtained value(s) is/(are) correct
+    problem = run_system(LCCICECost(ice_id="ice_1"), ivc)
+
+    assert problem.get_val(
+        "data:propulsion:he_power_train:ICE:ice_1:cost_per_engine", units="USD"
+    ) == pytest.approx(
+        81668.231,
+        rel=1e-2,
+    )
 
     problem.check_partials(compact_print=True)
