39b18a95ef00a1767655117c95ca05d2fc6c7298
johnathanasd <73662010+johnathanasd@users.noreply.github.com>
2025-04-15 11:51:06 +0200

    Add turboshaft operation cost

---
+++ src/fastga_he/models/propulsion/components/source/turboshaft/components/lcc_turboshaft_operation.py
@@ -0,0 +1,81 @@
+# This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
+# Electric Aircraft.
+# Copyright (C) 2025 ISAE-SUPAERO
+
+import numpy as np
+import openmdao.api as om
+
+
+class LCCTurboshaftOperation(om.ExplicitComponent):
+    """
+    Computation of the turboshaft engine operation cost. The TBO of based on the PT6s engines are
+    provided from
+    https://www.aopa.org/news-and-media/all-news/2020/february/24/turboprop-engine-repairs-for-less.
+    The Overhaul cost rate is obtained from
+    https://standardaero.com/engines/prattwhitneycanada/pt6a/pt6aflatrateoverhaulprogram/.
+    The yearly maintenance cost estimation is obtained from :cite:`gudmundsson:2013`.
+    """
+
+    def initialize(self):
+        self.options.declare(
+            name="turboshaft_id",
+            default=None,
+            desc="Identifier of the turboshaft",
+            allow_none=False,
+        )
+
+    def setup(self):
+        turboshaft_id = self.options["turboshaft_id"]
+
+        self.add_input(
+            name="data:TLAR:flight_hours_per_year",
+            val=283.2,
+            units="h",
+            desc="Expected number of hours flown per year",
+        )
+
+        self.add_input(
+            "data:propulsion:he_power_train:turboshaft:" + turboshaft_id + ":power_rating",
+            units="kW",
+            val=np.nan,
+        )
+
+        self.add_output(
+            name="data:propulsion:he_power_train:turboshaft:"
+            + turboshaft_id
+            + ":maintenance_per_unit",
+            units="USD/yr",
+            val=1e4,
+            desc="Annual Maintenance cost of the turboshaft",
+        )
+
+        self.declare_partials(of="*", wrt="*", method="exact")
+
+    def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
+        turboshaft_id = self.options["turboshaft_id"]
+
+        flight_mission = inputs["data:TLAR:flight_hours_per_year"]
+        power_rating = inputs[
+            "data:propulsion:he_power_train:turboshaft:" + turboshaft_id + ":power_rating"
+        ]
+
+        outputs[
+            "data:propulsion:he_power_train:turboshaft:" + turboshaft_id + ":maintenance_per_unit"
+        ] = flight_mission / 3500.0 * (1358.0 * np.log(power_rating) - 7480.0)
+
+    def compute_partials(self, inputs, partials, discrete_inputs=None):
+        turboshaft_id = self.options["turboshaft_id"]
+
+        flight_mission = inputs["data:TLAR:flight_hours_per_year"]
+        power_rating = inputs[
+            "data:propulsion:he_power_train:turboshaft:" + turboshaft_id + ":power_rating"
+        ]
+
+        partials[
+            "data:propulsion:he_power_train:turboshaft:" + turboshaft_id + ":maintenance_per_unit",
+            "data:propulsion:he_power_train:turboshaft:" + turboshaft_id + ":power_rating",
+        ] = flight_mission / 3500.0 * 1358.0 / power_rating
+        partials[
+            "data:propulsion:he_power_train:turboshaft:" + turboshaft_id + ":maintenance_per_unit",
+            "data:TLAR:flight_hours_per_year",
+        ] = (1358.0 * np.log(power_rating) - 7480.0) / 3500.0
