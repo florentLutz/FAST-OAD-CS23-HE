5893fa1cf3cdba8bb8480d3d219709d6288f2d5f
Florent <66668699+florentLutz@users.noreply.github.com>
2025-06-13 13:20:19 +0200

    Added fuel on main route
    PoC for identifying fuel on the main route (so without reserves !) because this is the quantity of interest for LCA. Is noy active by default for retrocomptability, so it means we will need to enable it in the mission directly. For later

--- src/fastga_he/models/propulsion/components/tanks/fuel_tanks/components/perf_fuel_tank.py
+++ src/fastga_he/models/propulsion/components/tanks/fuel_tanks/components/perf_fuel_tank.py
@@ -4,7 +4,8 @@
 
 import openmdao.api as om
 
-from ..components.perf_fuel_mission_consumed import PerformancesFuelConsumedMission
+from ..components.perf_fuel_consumed_mission import PerformancesFuelConsumedMission
+from ..components.perf_fuel_consumed_main_route import PerformancesFuelConsumedMainRoute
 from ..components.perf_fuel_remaining import PerformancesFuelRemainingMission
 
 
@@ -22,6 +23,12 @@ class PerformancesFuelTank(om.Group):
             "number_of_points", default=1, desc="number of equilibrium to be treated"
         )
         self.options.declare(
+            "number_of_points_reserve",
+            default=None,  # By default, for retro-compatibility, we want it disabled
+            desc="number of equilibrium to be treated in reserve",
+            types=int,
+        )
+        self.options.declare(
             name="fuel_tank_id",
             default=None,
             desc="Identifier of the fuel tank",
@@ -29,20 +36,31 @@ class PerformancesFuelTank(om.Group):
         )
 
     def setup(self):
-        number_of_points = self.options["number_of_points"]
-        fuel_tank_id = self.options["fuel_tank_id"]
-
         self.add_subsystem(
             "fuel_consumed_mission",
             PerformancesFuelConsumedMission(
-                number_of_points=number_of_points, fuel_tank_id=fuel_tank_id
+                number_of_points=self.options["number_of_points"],
+                fuel_tank_id=self.options["fuel_tank_id"],
             ),
             promotes=["*"],
         )
+
+        if self.options["number_of_points_reserve"]:
+            self.add_subsystem(
+                "fuel_consumed_main_route",
+                PerformancesFuelConsumedMainRoute(
+                    number_of_points=self.options["number_of_points"],
+                    fuel_tank_id=self.options["fuel_tank_id"],
+                    number_of_points_reserve=self.options["number_of_points_reserve"],
+                ),
+                promotes=["*"],
+            )
+
         self.add_subsystem(
             "fuel_remaining_mission",
             PerformancesFuelRemainingMission(
-                number_of_points=number_of_points, fuel_tank_id=fuel_tank_id
+                number_of_points=self.options["number_of_points"],
+                fuel_tank_id=self.options["fuel_tank_id"],
             ),
             promotes=["*"],
         )
