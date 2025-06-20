0f293e1cd52a78217401a911b8318ace02a74573
Florent <66668699+florentLutz@users.noreply.github.com>
2025-06-20 09:18:45 +0200

    More tests on the kodiak retrofit

--- integration_tests/quest_kodiak100/test_sizing_quest_kodiak100.py
+++ integration_tests/quest_kodiak100/test_sizing_quest_kodiak100.py
@@ -2,20 +2,25 @@
 # Electric Aircraft.
 # Copyright (C) 2022 ISAE-SUPAERO
 
+import os
 import os.path as pth
 from shutil import rmtree
 import logging
 
 import pytest
 
+import numpy as np
 import openmdao.api as om
 import fastoad.api as oad
+
+import plotly.graph_objects as go
 
 from utils.filter_residuals import filter_residuals
 
 DATA_FOLDER_PATH = pth.join(pth.dirname(__file__), "data")
 RESULTS_FOLDER_PATH = pth.join(pth.dirname(__file__), "results")
 WORKDIR_FOLDER_PATH = pth.join(pth.dirname(__file__), "workdir")
+RESULTS_SENSITIVITY_FOLDER_PATH = pth.join(pth.dirname(__file__), "results_sensitivity")
 
 
 @pytest.fixture(scope="module")
@@ -178,6 +183,172 @@ def test_retrofit_hybrid_kodiak():
     assert problem.get_val("data:environmental_impact:sizing:emission_factor") == pytest.approx(
         1.924, abs=1e-2
     )
+
+
+def test_retrofit_hybrid_kodiak_with_lca_varying_battery_lifespan():
+    """
+
+    We'll take a new turboshaft that correspond to the PW206B as it seems to have a fairly good
+    sfc according to https://en.wikipedia.org/wiki/Pratt_%26_Whitney_Canada_PW200. We'll use that
+    reference sfc as well as some educated on OPR and thermodynamic to get the right k_sfc before
+    we can get our hand on more data (possibly from Jane's). We'll consider that sfc is given at
+    Sea Level Static with power equal to limit power. This gives an k_sfc of 1.11
+
+    """
+
+    logging.basicConfig(level=logging.WARNING)
+    logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
+    logging.getLogger("fastoad.openmdao.variables.variable").disabled = True
+    logging.getLogger("bw2data").disabled = True
+    logging.getLogger("bw2calc").disabled = True
+
+    # Define used files depending on options
+    xml_file_name = "input_hybrid_kodiak_with_lca.xml"
+    process_file_name = "hybrid_kodiak_retrofit_with_lca.yml"
+
+    configurator = oad.FASTOADProblemConfigurator(pth.join(DATA_FOLDER_PATH, process_file_name))
+    problem = configurator.get_problem()
+
+    # Create inputs
+    ref_inputs = pth.join(DATA_FOLDER_PATH, xml_file_name)
+
+    problem.write_needed_inputs(ref_inputs)
+    problem.read_inputs()
+
+    # om.n2(problem, outfile=pth.join(RESULTS_FOLDER_PATH, "hybrid_kodiak_n2.html"))
+
+    # Change battery pack characteristics so that they match those of a high power,
+    # lower capacity cell like the Samsung INR18650-25R, we also take the weight fraction of the
+    # Pipistrel battery. Assumes same polarization curve
+    problem.model_options["*"] = {
+        "cell_capacity_ref": 2.5,
+        "cell_weight_ref": 45.0e-3,
+        "reference_curve_current": [500, 5000, 10000, 15000, 20000],
+        "reference_curve_relative_capacity": [1.0, 0.97, 1.0, 0.97, 0.95],
+    }
+
+    problem.setup()
+
+    problem.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_1:cell:c_rate_caliber",
+        val=8.0,
+        units="h**-1",
+    )
+
+    # om.n2(problem)
+
+    problem.run_model()
+
+    soc_start_array = np.linspace(100.0, 60.0, 20)
+    for soc_start in soc_start_array:
+        problem.set_val(
+            "data:propulsion:he_power_train:battery_pack:battery_pack_1:SOC_mission_start",
+            units="percent",
+            val=soc_start,
+        )
+
+        problem.run_model()
+
+        problem.output_file_path = pth.join(
+            RESULTS_SENSITIVITY_FOLDER_PATH, str(int(soc_start)) + "_soc_start_out.xml"
+        )
+        problem.write_outputs()
+
+
+def test_post_process_results():
+    fig = go.Figure()
+
+    socs_start_mission = []
+    single_scores = []
+    battery_mass_per_fu = []
+    battery_mass = []
+    flights_per_fu = []
+
+    generic_list = []
+
+    for file in os.listdir(RESULTS_SENSITIVITY_FOLDER_PATH):
+        datafile = oad.DataFile(pth.join(RESULTS_SENSITIVITY_FOLDER_PATH, file))
+        socs_start_mission.append(
+            datafile[
+                "data:propulsion:he_power_train:battery_pack:battery_pack_1:SOC_mission_start"
+            ].value[0]
+        )
+        single_scores.append(datafile["data:environmental_impact:single_score"].value[0])
+        battery_mass_per_fu.append(
+            datafile[
+                "data:propulsion:he_power_train:battery_pack:battery_pack_1:mass_per_fu"
+            ].value[0]
+        )
+        battery_mass.append(
+            datafile["data:propulsion:he_power_train:battery_pack:battery_pack_1:mass"].value[0]
+        )
+        generic_list.append(
+            datafile["data:propulsion:he_power_train:battery_pack:battery_pack_1:lifespan"].value[0]
+        )
+        flights_per_fu.append(
+            datafile["data:environmental_impact:flight_per_fu"].value[0]
+        )
+
+    sorted_single_scores = [single_scores[i] for i in np.argsort(socs_start_mission)]
+    sorted_battery_mass_per_fu = [battery_mass_per_fu[i] for i in np.argsort(socs_start_mission)]
+    flights_per_fu_sorted = [flights_per_fu[i] for i in np.argsort(socs_start_mission)]
+    sorted_socs_start = np.sort(socs_start_mission)
+
+    sorted_generic_list = [generic_list[i] for i in np.argsort(socs_start_mission)]
+    sorted_battery_mass = [battery_mass[i] for i in np.argsort(socs_start_mission)]
+
+    scatter = go.Scatter(
+        x=sorted_socs_start,
+        y=np.array(sorted_single_scores) / sorted_single_scores[-1],
+        mode="lines+markers",
+        name="Single score variation",
+        marker=dict(symbol="circle", size=10),
+    )
+    fig.add_trace(scatter)
+    scatter_2 = go.Scatter(
+        x=sorted_socs_start,
+        y=np.array(sorted_battery_mass_per_fu) / sorted_battery_mass_per_fu[-1],
+        mode="lines+markers",
+        name="Battery mass per FU variation",
+        marker=dict(symbol="square", size=10),
+    )
+    fig.add_trace(scatter_2)
+    scatter_3 = go.Scatter(
+        x=sorted_socs_start,
+        y=np.array(sorted_generic_list) / sorted_generic_list[-1],
+        mode="lines+markers",
+        name="Battery lifespan variation",
+        marker=dict(symbol="diamond", size=10),
+    )
+    fig.add_trace(scatter_3)
+    scatter_4 = go.Scatter(
+        x=sorted_socs_start,
+        y=np.array(sorted_battery_mass) / sorted_battery_mass[-1],
+        mode="lines+markers",
+        name="Battery mass variation",
+        marker=dict(symbol="cross", size=10),
+    )
+    fig.add_trace(scatter_4)
+    scatter_5 = go.Scatter(
+        x=sorted_socs_start,
+        y=flights_per_fu_sorted[-1] / np.array(flights_per_fu_sorted),
+        mode="lines+markers",
+        name="Functional unit per flight variation",
+        marker=dict(symbol="x", size=10),
+    )
+    fig.add_trace(scatter_5)
+
+    fig.update_layout(
+        title_text="Evolution of quantity of interest for the retrofit Kodiak case",
+        title_x=0.5,
+        xaxis_title="Initial SoC [%]",
+        height=800,
+        width=1600,
+        font_size=18,
+    )
+    fig.update_yaxes(title="Variation with respect to 100% SOC case")
+
+    fig.show()
 
 
 def test_retrofit_hybrid_kodiak_european_mix():
