a1366aa87bb59a1672a81ab080b9e26c04fc0311
johnathanasd <73662010+johnathanasd@users.noreply.github.com>
2025-07-01 18:22:21 +0200

    test not yet check, add default submodel for aero deltas

--- src/fastga_he/models/propulsion/assemblers/delta_cd_from_pt_file.py
+++ src/fastga_he/models/propulsion/assemblers/delta_cd_from_pt_file.py
@@ -1,6 +1,6 @@
 # This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
 # Electric Aircraft.
-# Copyright (C) 2022 ISAE-SUPAERO
+# Copyright (C) 2025 ISAE-SUPAERO
 
 import openmdao.api as om
 import numpy as np
@@ -10,10 +10,11 @@ from fastga_he.powertrain_builder.powertrain import FASTGAHEPowerTrainConfigurat
 
 from .constants import SUBMODEL_POWER_TRAIN_DELTA_CD
 
+DELTA_CD_FROM_PT = "fastga_he.submodel.propulsion.delta_cd.from_pt_file"
+oad.RegisterSubmodel.active_models[SUBMODEL_POWER_TRAIN_DELTA_CD] = DELTA_CD_FROM_PT
 
-@oad.RegisterSubmodel(
-    SUBMODEL_POWER_TRAIN_DELTA_CD, "fastga_he.submodel.propulsion.delta_cd.from_pt_file"
-)
+
+@oad.RegisterSubmodel(SUBMODEL_POWER_TRAIN_DELTA_CD, DELTA_CD_FROM_PT)
 class PowerTrainDeltaCdFromFile(om.ExplicitComponent):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
