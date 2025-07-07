bb1f09659fe52afa8d79663adacfd2a8f5563060
Florent <66668699+florentLutz@users.noreply.github.com>
2025-07-07 11:36:24 +0200

    Added full pipeline for 90 and 80 percent case
    As the title say, what was done in 2 tests before is done in one for the 90 and 80 percent case

--- integration_tests/pipistrel/test_lca_pipistrel.py
+++ integration_tests/pipistrel/test_lca_pipistrel.py
@@ -132,3 +132,223 @@ def test_lca_pipistrel_full_aging_effect():
     assert problem.get_val("data:environmental_impact:single_score") == pytest.approx(
         0.00096246, rel=1e-3
     )
+
+
+def test_lca_pipistrel_full_aging_full_pipeline_90_percent():
+    """
+    Tests that contains:
+     - A full sizing of a Pipistrel Velis Electro with an enforce of the cell number (not exactly the
+         reference case), but with a starting SOC of 90%
+     - A LCA evaluation of the sized configuration without considering the effect of the SOH on the
+         functional unit. The effect of the performances on the battery aging will however be
+         considered without any tuning.
+     - The LCA conf file will be automatically generated.
+     - Then said LCA wil be re-run with the proper effect of aging on the performances. Inputs for
+         that second LCA will be the output of the first part of the test, as is done in the first
+         two tests of that script. The only difference will be the number of hours flown per battery
+         which will be a results of another script
+    """
+
+    logging.basicConfig(level=logging.WARNING)
+    logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
+    logging.getLogger("fastoad.openmdao.variables.variable").disabled = True
+    logging.getLogger("bw2data").disabled = True
+    logging.getLogger("bw2calc").disabled = True
+
+    # Define used files depending on options
+    xml_file_name = "pipistrel_source_with_lca.xml"
+    process_file_name = "pipistrel_configuration_with_lca.yml"
+
+    configurator = oad.FASTOADProblemConfigurator(DATA_FOLDER_PATH / process_file_name)
+    problem = configurator.get_problem()
+
+    # Create inputs
+    ref_inputs = DATA_FOLDER_PATH / xml_file_name
+
+    # Setup the problem
+    problem.input_file_path = RESULTS_FOLDER_PATH / "pipistrel_in_with_lca_90_percent.xml"
+    problem.write_needed_inputs(ref_inputs)
+    problem.read_inputs()
+    problem.setup()
+
+    # Give good initial guess on a few key value to reduce the time it takes to converge
+    problem.set_val("data:weight:aircraft:MTOW", units="kg", val=600.0)
+    problem.set_val("data:weight:aircraft:OWE", units="kg", val=400.0)
+    problem.set_val("data:weight:aircraft:MZFW", units="kg", val=600.0)
+    problem.set_val("data:weight:aircraft:ZFW", units="kg", val=600.0)
+    problem.set_val("data:weight:aircraft:MLW", units="kg", val=600.0)
+
+    problem.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_1:SOC_mission_start",
+        units="percent",
+        val=90.0,
+    )
+    problem.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_2:SOC_mission_start",
+        units="percent",
+        val=90.0,
+    )
+
+    # Run the problem
+    problem.run_model()
+
+    # Write the outputs
+    problem.output_file_path = RESULTS_FOLDER_PATH / "pipistrel_out_with_lca_90_percent.xml"
+    problem.write_outputs()
+
+    assert problem.get_val("data:environmental_impact:single_score") == pytest.approx(
+        0.00094497, rel=1e-3
+    )
+
+    # Now we create the second problem with the proper aging effect
+
+    configurator_proper_aging = oad.FASTOADProblemConfigurator(
+        DATA_FOLDER_PATH / "pipistrel_configuration_with_lca_proper_aging.yml"
+    )
+    problem_proper_aging = configurator_proper_aging.get_problem()
+
+    # Create inputs
+    ref_inputs = problem.output_file_path
+
+    # Setup the problem
+    problem_proper_aging.input_file_path = (
+        RESULTS_FOLDER_PATH / "pipistrel_in_with_lca_90_percent_proper_aging.xml"
+    )
+    problem_proper_aging.write_needed_inputs(ref_inputs)
+    problem_proper_aging.read_inputs()
+    problem_proper_aging.setup()
+
+    # Results from a separate script
+    problem_proper_aging.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_1:hours_per_battery",
+        units="h",
+        val=365.25,
+    )
+    problem_proper_aging.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_2:hours_per_battery",
+        units="h",
+        val=365.25,
+    )
+
+    # Run the problem
+    problem_proper_aging.run_model()
+
+    # Write the outputs
+    problem_proper_aging.output_file_path = (
+        RESULTS_FOLDER_PATH / "pipistrel_out_with_lca_90_percent_proper_aging.xml"
+    )
+    problem_proper_aging.write_outputs()
+
+    assert problem_proper_aging.get_val("data:environmental_impact:single_score") == pytest.approx(
+        0.0010143792054993603, rel=1e-3
+    )
+
+
+def test_lca_pipistrel_full_aging_full_pipeline_80_percent():
+    """
+    Tests that contains:
+     - A full sizing of a Pipistrel Velis Electro with an enforce of the cell number (not exactly the
+         reference case), but with a starting SOC of 80%
+     - A LCA evaluation of the sized configuration without considering the effect of the SOH on the
+         functional unit. The effect of the performances on the battery aging will however be
+         considered without any tuning.
+     - The LCA conf file will be automatically generated.
+     - Then said LCA wil be re-run with the proper effect of aging on the performances. Inputs for
+         that second LCA will be the output of the first part of the test, as is done in the first
+         two tests of that script. The only difference will be the number of hours flown per battery
+         which will be a results of another script
+    """
+
+    logging.basicConfig(level=logging.WARNING)
+    logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
+    logging.getLogger("fastoad.openmdao.variables.variable").disabled = True
+    logging.getLogger("bw2data").disabled = True
+    logging.getLogger("bw2calc").disabled = True
+
+    # Define used files depending on options
+    xml_file_name = "pipistrel_source_with_lca.xml"
+    process_file_name = "pipistrel_configuration_with_lca.yml"
+
+    configurator = oad.FASTOADProblemConfigurator(DATA_FOLDER_PATH / process_file_name)
+    problem = configurator.get_problem()
+
+    # Create inputs
+    ref_inputs = DATA_FOLDER_PATH / xml_file_name
+
+    # Setup the problem
+    problem.input_file_path = RESULTS_FOLDER_PATH / "pipistrel_in_with_lca_80_percent.xml"
+    problem.write_needed_inputs(ref_inputs)
+    problem.read_inputs()
+    problem.setup()
+
+    # Give good initial guess on a few key value to reduce the time it takes to converge
+    problem.set_val("data:weight:aircraft:MTOW", units="kg", val=600.0)
+    problem.set_val("data:weight:aircraft:OWE", units="kg", val=400.0)
+    problem.set_val("data:weight:aircraft:MZFW", units="kg", val=600.0)
+    problem.set_val("data:weight:aircraft:ZFW", units="kg", val=600.0)
+    problem.set_val("data:weight:aircraft:MLW", units="kg", val=600.0)
+
+    problem.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_1:SOC_mission_start",
+        units="percent",
+        val=80.0,
+    )
+    problem.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_2:SOC_mission_start",
+        units="percent",
+        val=80.0,
+    )
+
+    # Run the problem
+    problem.run_model()
+
+    # Write the outputs
+    problem.output_file_path = RESULTS_FOLDER_PATH / "pipistrel_out_with_lca_80_percent.xml"
+    problem.write_outputs()
+
+    assert problem.get_val("data:environmental_impact:single_score") == pytest.approx(
+        0.00100379, rel=1e-3
+    )
+
+    # Now we create the second problem with the proper aging effect
+
+    configurator_proper_aging = oad.FASTOADProblemConfigurator(
+        DATA_FOLDER_PATH / "pipistrel_configuration_with_lca_proper_aging.yml"
+    )
+    problem_proper_aging = configurator_proper_aging.get_problem()
+
+    # Create inputs
+    ref_inputs = problem.output_file_path
+
+    # Setup the problem
+    problem_proper_aging.input_file_path = (
+        RESULTS_FOLDER_PATH / "pipistrel_in_with_lca_80_percent_proper_aging.xml"
+    )
+    problem_proper_aging.write_needed_inputs(ref_inputs)
+    problem_proper_aging.read_inputs()
+    problem_proper_aging.setup()
+
+    # Results from a separate script
+    problem_proper_aging.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_1:hours_per_battery",
+        units="h",
+        val=373.82,
+    )
+    problem_proper_aging.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_2:hours_per_battery",
+        units="h",
+        val=373.82,
+    )
+
+    # Run the problem
+    problem_proper_aging.run_model()
+
+    # Write the outputs
+    problem_proper_aging.output_file_path = (
+        RESULTS_FOLDER_PATH / "pipistrel_out_with_lca_80_percent_proper_aging.xml"
+    )
+    problem_proper_aging.write_outputs()
+
+    assert problem_proper_aging.get_val("data:environmental_impact:single_score") == pytest.approx(
+        0.00116834, rel=1e-3
+    )
