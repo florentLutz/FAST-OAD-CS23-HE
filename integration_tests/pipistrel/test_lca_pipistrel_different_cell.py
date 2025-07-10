5466ec2a48b6ae43686b07149716d0eebf09a7ab
Florent <66668699+florentLutz@users.noreply.github.com>
2025-07-10 11:22:32 +0200

    Added high lifespan case + small mods
    Starting with the small modifcations: to ensure stability of the convergence process, the CG range for the aircraft is no longer computed rather it is an input whose value correspond to the reference case. Note that this means we are likely being conservative for the high lifespan case because batteries are heavier so the relative variation due to pax loading will likely e smaller.
    Also added a new test case with batteries that are heavy but have very long lifespan (to the point where for a Pipistrel they wouldn't need to be replaced). Although these batteries have very low voltage (gor the high energy density case it was pretty similar to default case) so next commit will be about implementing a easier submodel

--- integration_tests/pipistrel/test_lca_pipistrel_different_cell.py
+++ integration_tests/pipistrel/test_lca_pipistrel_different_cell.py
@@ -82,7 +82,7 @@ def test_lca_pipistrel_high_energy_density_cell():
     - A LCA evaluation of the sized configuration without considering the effect of the SOH on the
         functional unit. The effect of the performances on the battery aging will however be
         considered without any tuning.
-    - The LCA configuration file will has been written separately to represent the change in battery
+    - The LCA configuration file will be written separately to represent the change in battery
         production process.
     - Battery production process for Si-NMC battery is not available in EcoInvent, so we will use
         the surrogate process proposed in :cite:`pollet:2023`. This surrogate however assumes that
@@ -168,4 +168,119 @@ def test_lca_pipistrel_high_energy_density_cell():
 
     assert problem.get_val("data:environmental_impact:single_score") == pytest.approx(
         0.00789594, rel=1e-3
+    )
+
+
+def test_lca_pipistrel_long_lifespan_cell():
+    """
+    Tests that contains:
+    - A full sizing of a Pipistrel Velis Electro with an enforce of the cell number (not exactly the
+        reference case), but with a starting SOC of 100%, with a higher energy density cell but with
+        lower lifetime.
+    - A LCA evaluation of the sized configuration without considering the effect of the SOH on the
+        functional unit. The effect of the performances on the battery aging will however be
+        considered without any tuning.
+    - The LCA configuration file will be written separately to represent the change in battery
+        production process.
+    - Battery production process for Skeleton SuperBattery is not available in EcoInvent, so we will
+        use a surrogate process inspired from what is done in :cite:`pollet:2023`. This surrogate
+        assumes that producing a cathode in graphite is the same as producing an anode in graphite.
+    - A small observation to be made is that in the 3.9.1 version of EcoInvent the impact of
+        graphite anode production per kg is around 4 times lower than the production of the cathode
+        in the reference cell per kg, meaning the impact of cell production will be smaller. In more
+        recent version of EcoInvent those two process have the same order of magnitude for the
+        impacts so we might be underestimating the impacts of battery production.
+    - As is the case in application where the battery mass is very high, the OAD process oscillates
+        a lot in the last iteration which actually increases the residuals. To stop them at a
+        reasonable value, the tolerance has been decreased.
+    """
+
+    logging.basicConfig(level=logging.WARNING)
+    logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
+    logging.getLogger("fastoad.openmdao.variables.variable").disabled = True
+    logging.getLogger("bw2data").disabled = True
+    logging.getLogger("bw2calc").disabled = True
+
+    # Define used files depending on options
+    xml_file_name = "pipistrel_source_with_lca.xml"
+    process_file_name = "pipistrel_configuration_with_lca_high_lifespan_cell.yml"
+
+    configurator = oad.FASTOADProblemConfigurator(DATA_FOLDER_PATH / process_file_name)
+    problem = configurator.get_problem()
+
+    # Create inputs
+    ref_inputs = DATA_FOLDER_PATH / xml_file_name
+
+    # Setup the problem
+    problem.write_needed_inputs(ref_inputs)
+    problem.read_inputs()
+
+    # In addition to a change in battery production process, the characteristics of the battery
+    # packs are changed. We will assume same polarization curve and small relative capacity effect
+    # . We need at least 4 "fake" points for the relative capacity effect as it assumed to be a
+    # deg 3 polynomial.
+    # The code seems to have a hard time converging with "big" cells, so instead of having a few
+    # "big" cells we'll have more smaller one
+    problem.model_options["*"] = {
+        "cell_capacity_ref": 2.30,
+        "cell_weight_ref": 75.0e-3,
+        "reference_curve_current": [100., 10000., 30000., 46000.],
+        "reference_curve_relative_capacity": [1.0, 0.99, 0.98, 0.97],
+    }
+
+    problem.setup()
+
+    # Give good initial guess on a few key value to reduce the time it takes to converge
+    problem.set_val("data:weight:aircraft:MTOW", units="kg", val=1500.0)
+    problem.set_val("data:weight:aircraft:OWE", units="kg", val=1300.0)
+    problem.set_val("data:weight:aircraft:MZFW", units="kg", val=1500.0)
+    problem.set_val("data:weight:aircraft:ZFW", units="kg", val=1500.0)
+    problem.set_val("data:weight:aircraft:MLW", units="kg", val=1500.0)
+
+    # In addition to a change in battery production process, the lifespan of the batteries is also
+    # affected, and they need replacement way less often.
+    problem.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_1:lifespan", val=50000.0
+    )
+    problem.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_2:lifespan", val=50000.0
+    )
+
+    problem.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_1:cell:c_rate_caliber",
+        val=20.0,
+        units="h**-1",
+    )
+    problem.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_2:cell:c_rate_caliber",
+        val=20.0,
+        units="h**-1",
+    )
+
+    # To aid convergence
+    problem.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_1:number_modules", val=10.0
+    )
+    problem.set_val(
+        "data:propulsion:he_power_train:battery_pack:battery_pack_1:number_modules", val=10.0
+    )
+
+    # Run the problem
+    problem.run_model()
+
+    # Write the outputs
+    problem.write_outputs()
+
+    # assert problem.get_val("data:weight:aircraft:MTOW") == pytest.approx(494, abs=1)
+    # assert problem.get_val("data:weight:aircraft:OWE") == pytest.approx(322, abs=1)
+    #
+    # assert problem.get_val(
+    #     "data:propulsion:he_power_train:battery_pack:battery_pack_1:mass", units="kg"
+    # ) == pytest.approx(38.8, rel=1e-2)
+    # assert problem.get_val(
+    #     "data:propulsion:he_power_train:battery_pack:battery_pack_2:mass", units="kg"
+    # ) == pytest.approx(38.8, rel=1e-2)
+
+    assert problem.get_val("data:environmental_impact:single_score") == pytest.approx(
+        0.00118114, rel=1e-3
     )
