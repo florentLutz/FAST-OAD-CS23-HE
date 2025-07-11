dc7063183015761c5fbbd59405bb682a61f9b508
Florent <66668699+florentLutz@users.noreply.github.com>
2025-07-11 16:11:09 +0200

    Pipistrel club heavy with built-in LCA

--- integration_tests/pipistrel/test_lca_pipistrel_club.py
+++ integration_tests/pipistrel/test_lca_pipistrel_club.py
@@ -8,9 +8,6 @@ import logging
 
 import pytest
 
-import numpy as np
-import openmdao.api as om
-
 import fastoad.api as oad
 
 DATA_FOLDER_PATH = pathlib.Path(__file__).parent / "data_lca"
@@ -61,4 +58,57 @@ def test_lca_pipistrel_sw_121_reference():
 
     assert problem.get_val("data:environmental_impact:single_score") == pytest.approx(
         0.00636904, rel=1e-3
+    )
+    assert problem.get_val("data:weight:aircraft:MTOW", units="kg") == pytest.approx(
+        589.0, rel=1e-3
+    )
+
+
+def test_lca_pipistrel_sw_121_heavy():
+    """
+    Tests that contains:
+    - A full sizing of a Pipistrel SW121
+    - A LCA evaluation of the sized configuration
+    - The LCA configuration file will be automatically generated.
+    """
+
+    logging.basicConfig(level=logging.WARNING)
+    logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
+    logging.getLogger("fastoad.openmdao.variables.variable").disabled = True
+    logging.getLogger("bw2data").disabled = True
+    logging.getLogger("bw2calc").disabled = True
+
+    # Define used files depending on options
+    xml_file_name = "pipistrel_club_heavy_source_with_lca.xml"
+    process_file_name = "pipistrel_club_heavy_configuration_with_lca.yml"
+
+    configurator = oad.FASTOADProblemConfigurator(DATA_FOLDER_PATH / process_file_name)
+    problem = configurator.get_problem()
+
+    # Create inputs
+    ref_inputs = DATA_FOLDER_PATH / xml_file_name
+
+    # Setup the problem
+    problem.write_needed_inputs(ref_inputs)
+    problem.read_inputs()
+    problem.setup()
+
+    # Give good initial guess on a few key value to reduce the time it takes to converge
+    problem.set_val("data:weight:aircraft:MTOW", units="kg", val=600.0)
+    problem.set_val("data:weight:aircraft:OWE", units="kg", val=400.0)
+    problem.set_val("data:weight:aircraft:MZFW", units="kg", val=600.0)
+    problem.set_val("data:weight:aircraft:ZFW", units="kg", val=600.0)
+    problem.set_val("data:weight:aircraft:MLW", units="kg", val=600.0)
+
+    # Run the problem
+    problem.run_model()
+
+    # Write the outputs
+    problem.write_outputs()
+
+    assert problem.get_val("data:environmental_impact:single_score") == pytest.approx(
+        0.00623929, rel=1e-3
+    )
+    assert problem.get_val("data:weight:aircraft:MTOW", units="kg") == pytest.approx(
+        640.0, rel=1e-3
     )
