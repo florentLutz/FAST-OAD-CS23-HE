d61b7ed4576044c9b70ff120f265a8ae59aabcb4
Florent <66668699+florentLutz@users.noreply.github.com>
2025-07-21 12:07:22 +0200

    Inital run for electric Cirrus SR22

--- integration_tests/cirrus_sr22/test_sizing_cirrus_sr22.py
+++ integration_tests/cirrus_sr22/test_sizing_cirrus_sr22.py
@@ -65,3 +65,40 @@ def test_sizing_sr22(cleanup):
     assert problem.get_val("data:mission:sizing:fuel", units="kg") == pytest.approx(
         234.00, rel=1e-2
     )
+
+
+def test_sizing_sr22_electric(cleanup):
+    """Test the overall aircraft design process with wing positioning under VLM method."""
+    logging.basicConfig(level=logging.WARNING)
+    logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
+    logging.getLogger("fastoad.openmdao.variables.variable").disabled = True
+
+    # Define used files depending on options
+    xml_file_name = "input_sr22_electric.xml"
+    process_file_name = "full_sizing_electric.yml"
+
+    configurator = oad.FASTOADProblemConfigurator(pth.join(DATA_FOLDER_PATH, process_file_name))
+    problem = configurator.get_problem()
+
+    # Create inputs
+    ref_inputs = pth.join(DATA_FOLDER_PATH, xml_file_name)
+    # n2_path = pth.join(RESULTS_FOLDER_PATH, "n2_cirrus.html")
+    # api.list_modules(pth.join(DATA_FOLDER_PATH, process_file_name), force_text_output=True)
+
+    problem.write_needed_inputs(ref_inputs)
+    problem.read_inputs()
+    problem.setup()
+
+    # om.n2(problem, show_browser=False, outfile=n2_path)
+
+    problem.run_model()
+
+    _, _, residuals = problem.model.get_nonlinear_vectors()
+    residuals = filter_residuals(residuals)
+
+    problem.write_outputs()
+
+    assert problem.get_val("data:weight:aircraft:MTOW", units="kg") == pytest.approx(
+        1601.0, rel=1e-2
+    )
+    assert problem.get_val("data:weight:aircraft:OWE", units="kg") == pytest.approx(992.0, rel=1e-2)
