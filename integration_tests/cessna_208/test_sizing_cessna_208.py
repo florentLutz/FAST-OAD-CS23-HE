eb8eaf9154154c973cb7864a86ad1308795c6621
Florent <66668699+florentLutz@users.noreply.github.com>
2025-09-03 11:17:19 +0200

    First run hybrid cessna

--- integration_tests/cessna_208/test_sizing_cessna_208.py
+++ integration_tests/cessna_208/test_sizing_cessna_208.py
@@ -18,7 +18,7 @@ RESULTS_FOLDER_PATH = pathlib.Path(__file__).parent / "results"
 def test_sizing_cessna_208b():
     """
     We will not do the latest version of the C208 rather we'll do the C208 before the re-engining so
-    it will be rated at 600hp.
+    it will be rated at 675hp.
     """
     logging.basicConfig(level=logging.WARNING)
     logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
@@ -55,6 +55,38 @@ def test_sizing_cessna_208b():
     assert problem.get_val("data:weight:aircraft:OWE", units="kg") == pytest.approx(
         2146.0, rel=5e-2
     )  # From the POH found online + weight of seats at around 100kg
+    assert problem.get_val("data:weight:aircraft:MTOW", units="kg") == pytest.approx(
+        3968.0, rel=5e-2
+    )
+
+def test_sizing_hybrid_cessna_208b():
+    """
+    Parallel hybrid version of the c208b, in this first version we won't allow resizing of the
+    turboshaft, we'll just "replace" thermal power by electric power. Designed for 200nm
+    """
+
+    logging.basicConfig(level=logging.WARNING)
+    logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
+    logging.getLogger("fastoad.openmdao.variables.variable").disabled = True
+
+    # Define used files depending on options
+    xml_file_name = "input_hybrid_c208_retrofit.xml"
+    process_file_name = "hybrid_c208_retrofit.yml"
+
+    configurator = oad.FASTOADProblemConfigurator(DATA_FOLDER_PATH / process_file_name)
+    problem = configurator.get_problem()
+
+    # Create inputs
+    ref_inputs = DATA_FOLDER_PATH / xml_file_name
+
+    problem.write_needed_inputs(ref_inputs)
+    problem.read_inputs()
+    problem.setup()
+
+    problem.run_model()
+
+    problem.write_outputs()
+
     assert problem.get_val("data:weight:aircraft:MTOW", units="kg") == pytest.approx(
         3968.0, rel=5e-2
     )
