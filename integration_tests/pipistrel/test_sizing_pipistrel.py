d530285629cfc1833ae7eca59e68acc32c2fdb04
johnathanasd <73662010+johnathanasd@users.noreply.github.com>
2025-09-09 16:59:33 +0200

    restore test

--- integration_tests/pipistrel/test_sizing_pipistrel.py
+++ integration_tests/pipistrel/test_sizing_pipistrel.py
@@ -763,39 +763,6 @@ def test_aircraft_geometry_plot():
     fig.write_image(pth.join(RESULTS_FOLDER_PATH, "pipistrel_geometry.pdf"))
 
 
-def test_aircraft_geometry_plot_ATR42():
-    results_pipistrel_file_path = pth.join(DATA_FOLDER_PATH, "problem_outputs.xml")
-
-    fig = aircraft_geometry_plot(results_pipistrel_file_path, name="ATR42")
-
-    fig.update_layout(
-        height=800,
-        width=1600,
-        font_size=18,
-    )
-
-    pipistrel_top_view = Image.open("data/ATR42_topview.jpg")
-
-    fig.add_layout_image(
-        dict(
-            source=pipistrel_top_view,
-            xref="x",
-            yref="y",
-            y=6.47,
-            x=-10.71 / 2,
-            sizex=10.71,
-            sizey=6.47,
-            sizing="stretch",
-            opacity=0.75,
-            layer="below",
-        )
-    )
-
-    # fig.show()
-    fig.write_image(pth.join(RESULTS_FOLDER_PATH, "ATR42_geometry.svg"))
-    fig.write_image(pth.join(RESULTS_FOLDER_PATH, "ATR42_geometry.pdf"))
-
-
 def test_plot_power_profile_results():
     mission_data_file = pth.join(RESULTS_FOLDER_PATH, "mission_data.csv")
     pt_data_file = pth.join(RESULTS_FOLDER_PATH, "pipistrel_power_train_data.csv")
