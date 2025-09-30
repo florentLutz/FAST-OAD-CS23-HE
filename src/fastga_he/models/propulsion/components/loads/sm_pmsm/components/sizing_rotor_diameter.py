adfbbe738aef6e7bfa17be7d64b7e5ce1d6a531e
johnathanasd <73662010+johnathanasd@users.noreply.github.com>
2025-09-30 09:21:46 +0200

    redo rotor diameter

--- src/fastga_he/models/propulsion/components/loads/sm_pmsm/components/sizing_rotor_diameter.py
+++ src/fastga_he/models/propulsion/components/loads/sm_pmsm/components/sizing_rotor_diameter.py
@@ -9,7 +9,7 @@ import openmdao.api as om
 class SizingRotorDiameter(om.ExplicitComponent):
     """
     Computation of the rotor diameterof a cylindrical PMSM. The formulas are obtained from
-    equation (II-81) and (II-89) in :cite:`touhami:2020`.
+    equation (II-85) in :cite:`touhami:2020`.
     """
 
     def initialize(self):
@@ -20,12 +20,6 @@ class SizingRotorDiameter(om.ExplicitComponent):
     def setup(self):
         motor_id = self.options["motor_id"]
 
-        self.add_input(
-            name="data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":bore_diameter",
-            val=np.nan,
-            units="m",
-            desc="Stator bore diameter of the SM PMSM",
-        )
         self.add_input(
             name="data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":end_winding_coeff",
             val=np.nan,
@@ -38,7 +32,9 @@ class SizingRotorDiameter(om.ExplicitComponent):
             desc="Max continuous rpm of the motor",
         )
         self.add_input(
-            name="data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":form_coefficient",
+            name="data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":active_length",
+            units="m",
+            desc="The length of electromagnetism active part of SM PMSM",
             val=np.nan,
         )
         self.add_input(
@@ -68,12 +64,9 @@ class SizingRotorDiameter(om.ExplicitComponent):
         end_winding_coeff = inputs[
             "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":end_winding_coeff"
         ]
-        bore_diameter = inputs[
-            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":bore_diameter"
-        ]
         rpm_rating = inputs["data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":rpm_rating"]
-        form_coefficient = inputs[
-            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":form_coefficient"
+        active_length = inputs[
+            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":active_length"
         ]
         rho_rotor = inputs[
             "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":rotor_material_density"
@@ -81,32 +74,83 @@ class SizingRotorDiameter(om.ExplicitComponent):
         e_rotor = inputs["data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":youngs_modules"]
 
         outputs["data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":rotor_diameter"] = (
-            (bore_diameter * end_winding_coeff / form_coefficient) ** 2.0
-            * (np.pi * rpm_rating / 20.0)
-            * np.sqrt(5.0 * rho_rotor / (6.0 * e_rotor))
+            0.25
+            * rpm_rating
+            * np.pi
+            * active_length**2.0
+            * end_winding_coeff**2.0
+            / np.sqrt(30.0 * e_rotor / rho_rotor)
         )
 
     def compute_partials(self, inputs, partials, discrete_inputs=None):
         motor_id = self.options["motor_id"]
 
-        partials[
-            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":rotor_diameter",
-            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":radius_ratio",
-        ] = inputs["data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":bore_diameter"]
+        end_winding_coeff = inputs[
+            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":end_winding_coeff"
+        ]
+        rpm_rating = inputs["data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":rpm_rating"]
+        active_length = inputs[
+            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":active_length"
+        ]
+        rho_rotor = inputs[
+            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":rotor_material_density"
+        ]
+        e_rotor = inputs["data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":youngs_modules"]
 
         partials[
             "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":rotor_diameter",
-            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":bore_diameter",
-        ] = inputs["data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":radius_ratio"]
-
-        partials[
-            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":air_gap_thickness",
-            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":radius_ratio",
-        ] = (-inputs["data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":bore_diameter"]) / 2.0
-
-        partials[
-            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":air_gap_thickness",
-            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":bore_diameter",
+            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":end_winding_coeff",
         ] = (
-            1.0 - inputs["data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":radius_ratio"]
-        ) / 2.0
+            0.5
+            * rpm_rating
+            * np.pi
+            * active_length**2.0
+            * end_winding_coeff
+            / np.sqrt(30.0 * e_rotor / rho_rotor)
+        )
+
+        partials[
+            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":rotor_diameter",
+            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":active_length",
+        ] = (
+            0.5
+            * rpm_rating
+            * np.pi
+            * active_length
+            * end_winding_coeff**2.0
+            / np.sqrt(30.0 * e_rotor / rho_rotor)
+        )
+
+        partials[
+            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":rotor_diameter",
+            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":rpm_rating",
+        ] = (
+            0.25
+            * np.pi
+            * active_length**2.0
+            * end_winding_coeff**2.0
+            / np.sqrt(30.0 * e_rotor / rho_rotor)
+        )
+
+        partials[
+            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":rotor_diameter",
+            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":youngs_modules",
+        ] = (
+            -0.25
+            * np.pi
+            * active_length**2.0
+            * end_winding_coeff**2.0
+            / (2.0 * rho_rotor * np.sqrt(30.0) * np.sqrt(e_rotor / rho_rotor) ** 3.0)
+        )
+
+        partials[
+            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":rotor_diameter",
+            "data:propulsion:he_power_train:SM_PMSM:" + motor_id + ":rotor_material_density",
+        ] = (
+            0.25
+            * e_rotor
+            * np.pi
+            * active_length**2.0
+            * end_winding_coeff**2.0
+            / (2.0 * rho_rotor**2.0 * np.sqrt(30.0) * np.sqrt(e_rotor / rho_rotor) ** 3.0)
+        )
