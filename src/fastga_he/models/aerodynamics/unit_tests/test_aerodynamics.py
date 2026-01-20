8f969a12ae55c3353c26a5516164bb266e86b8c6
Florent <66668699+florentLutz@users.noreply.github.com>
2026-01-20 10:02:47 +0100

    Fuselage CD0 RTA + tests

--- src/fastga_he/models/aerodynamics/unit_tests/test_aerodynamics.py
+++ src/fastga_he/models/aerodynamics/unit_tests/test_aerodynamics.py
@@ -8,6 +8,7 @@ import numpy as np
 import openmdao.api as om
 
 from ..components.cd0_wing_rta import Cd0Wing
+from ..components.cd0_fuselage_rta import Cd0Fuselage
 
 from tests.testing_utilities import run_system
 
@@ -30,6 +31,29 @@ def test_cd0_wing_rta():
 
     assert problem.get_val("data:aerodynamics:wing:cruise:CD0") == pytest.approx(
         0.01013465, rel=1e-2
+    )
+
+    problem.check_partials(compact_print=True)
+
+
+def test_cd0_fuselage_rta():
+    ivc = om.IndepVarComp()
+    ivc.add_output("data:geometry:fuselage:length", val=22.39, units="m")
+    ivc.add_output("data:geometry:fuselage:maximum_height", val=2.93, units="m")
+    ivc.add_output("data:geometry:fuselage:maximum_width", val=2.79, units="m")
+    ivc.add_output("data:geometry:fuselage:wetted_area", val=166.018, units="m**2")
+    ivc.add_output("data:geometry:wing:area", val=57.2, units="m**2")
+    ivc.add_output("data:aerodynamics:wing:cruise:reynolds", val=6480282, units="unitless")
+    ivc.add_output(
+        "data:aerodynamics:aircraft:cruise:CL", val=np.linspace(0, 1.49, 150), units="unitless"
+    )
+    ivc.add_output("data:TLAR:cruise_mach", val=0.5, units="unitless")
+
+    # Run problem and check obtained value(s) is/(are) correct
+    problem = run_system(Cd0Fuselage(), ivc)
+
+    assert problem.get_val("data:aerodynamics:fuselage:cruise:CD0") == pytest.approx(
+        0.00650617, rel=1e-2
     )
 
     problem.check_partials(compact_print=True)
