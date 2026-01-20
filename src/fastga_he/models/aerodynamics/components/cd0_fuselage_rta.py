d72ad4bb15845154073fbf7452890ac6981a95eb
Florent LUTZ <66668699+florentLutz@users.noreply.github.com>
2026-01-20 15:19:07 +0100

    Update src/fastga_he/models/aerodynamics/components/cd0_fuselage_rta.py
    Co-authored-by: johnathanasd <73662010+johnathanasd@users.noreply.github.com>

--- src/fastga_he/models/aerodynamics/components/cd0_fuselage_rta.py
+++ src/fastga_he/models/aerodynamics/components/cd0_fuselage_rta.py
@@ -1,7 +1,7 @@
 """Computation of form drag for fuselage."""
 # This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
 # Electric Aircraft.
-# Copyright (C) 2025 ISAE-SUPAERO
+# Copyright (C) 2026 ISAE-SUPAERO
 
 import numpy as np
 import openmdao.api as om
