c1d35ba348873faa9b08fa82039382c8083a48bb
Florent <66668699+florentLutz@users.noreply.github.com>
2026-01-20 08:40:07 +0100

    Test for CD0 wing RTA integration

---
+++ src/fastga_he/models/aerodynamics/unit_tests/test_aerodynamics.py
@@ -0,0 +1,33 @@
+#  This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
+#  Electric Aircraft.
+#  Copyright (C) 2026 ISAE-SUPAERO
+
+import pytest
+
+import numpy as np
+import openmdao.api as om
+
+from ..components.cd0_wing_rta import Cd0Wing
+
+from tests.testing_utilities import run_system
+
+
+def test_cd0_wing_rta():
+    ivc = om.IndepVarComp()
+    ivc.add_output("data:geometry:wing:MAC:length", val=2.3, units="m")
+    ivc.add_output("data:geometry:wing:area", val=57.2, units="m**2")
+    ivc.add_output("data:geometry:wing:wetted_area", val=102.0, units="m**2")
+    ivc.add_output("data:geometry:wing:sweep_25", val=0.04, units="rad")
+    ivc.add_output("data:geometry:wing:thickness_ratio", val=0.137, units="unitless")
+    ivc.add_output("data:aerodynamics:wing:cruise:reynolds", val=6480282, units="unitless")
+    ivc.add_output(
+        "data:aerodynamics:aircraft:cruise:CL", val=np.linspace(0, 1.49, 150), units="unitless"
+    )
+    ivc.add_output("data:TLAR:cruise_mach", val=0.5, units="unitless")
+
+    # Run problem and check obtained value(s) is/(are) correct
+    problem = run_system(Cd0Wing(), ivc)
+
+    assert problem.get_val("data:aerodynamics:wing:cruise:CD0") == pytest.approx(0.01013465, rel=1e-2)
+
+    problem.check_partials(compact_print=True)
