e5c526f0f61f2c4e8659a91b3b8f5513308d3088
johnathanasd <73662010+johnathanasd@users.noreply.github.com>
2026-01-27 10:23:01 +0100

    Remove unused files and adjust fuel consumption

--- integration_tests/dhc_6_twin_otter/test_sizing_dhc6_twin_otter.py
+++ integration_tests/dhc_6_twin_otter/test_sizing_dhc6_twin_otter.py
@@ -2,7 +2,7 @@
 # Electric Aircraft.
 # Copyright (C) 2026 ISAE-SUPAERO
 
-import os.path as pth
+import pathlib
 from shutil import rmtree
 import logging
 
@@ -13,11 +13,9 @@ import fastoad.api as oad
 
 from utils.filter_residuals import filter_residuals
 
-from fastga_he.gui.power_train_network_viewer import power_train_network_viewer
-
-DATA_FOLDER_PATH = pth.join(pth.dirname(__file__), "data")
-RESULTS_FOLDER_PATH = pth.join(pth.dirname(__file__), "results")
-WORKDIR_FOLDER_PATH = pth.join(pth.dirname(__file__), "workdir")
+DATA_FOLDER_PATH = pathlib.Path(__file__).parent / "data"
+RESULTS_FOLDER_PATH = pathlib.Path(__file__).parent / "results"
+WORKDIR_FOLDER_PATH = pathlib.Path(__file__).parent / "workdir"
 
 
 @pytest.fixture(scope="module")
@@ -37,13 +35,13 @@ def test_sizing_dhc6_twin_otter():
     xml_file_name = "input_dhc6_twin_otter.xml"
     process_file_name = "full_sizing_dhc6_twin_otter.yml"
 
-    configurator = oad.FASTOADProblemConfigurator(pth.join(DATA_FOLDER_PATH, process_file_name))
+    configurator = oad.FASTOADProblemConfigurator(DATA_FOLDER_PATH / process_file_name)
     problem = configurator.get_problem()
 
     # Create inputs
-    ref_inputs = pth.join(DATA_FOLDER_PATH, xml_file_name)
-    n2_path = pth.join(RESULTS_FOLDER_PATH, "n2_dhc6_twin_otter.html")
-    # api.list_modules(pth.join(DATA_FOLDER_PATH, process_file_name), force_text_output=True)
+    ref_inputs = DATA_FOLDER_PATH / xml_file_name
+    n2_path = RESULTS_FOLDER_PATH / "n2_dhc6_twin_otter.html"
+    # api.list_modules(DATA_FOLDER_PATH /  process_file_name, force_text_output=True)
 
     problem.model_options["*propeller_*"] = {"mass_as_input": True}
 
@@ -70,44 +68,5 @@ def test_sizing_dhc6_twin_otter():
         3320.0, rel=5e-2
     )
     assert problem.get_val("data:mission:sizing:fuel", units="kg") == pytest.approx(
-        760.00, rel=5e-2
+        808.00, rel=5e-2
     )
-
-
-def test_operational_mission_dhc6_twin_otter():
-    """Test the overall aircraft design process with wing positioning."""
-    logging.basicConfig(level=logging.WARNING)
-    logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
-    logging.getLogger("fastoad.openmdao.variables.variable").disabled = True
-
-    # Define used files depending on options
-    xml_file_name = "input_dhc6_twin_otter_op_mission.xml"
-    process_file_name = "operational_mission_dhc6_twin_otter.yml"
-
-    configurator = oad.FASTOADProblemConfigurator(pth.join(DATA_FOLDER_PATH, process_file_name))
-    problem = configurator.get_problem()
-
-    # Create inputs
-    ref_inputs = pth.join(DATA_FOLDER_PATH, xml_file_name)
-
-    problem.write_needed_inputs(ref_inputs)
-    problem.read_inputs()
-    problem.setup()
-
-    problem.run_model()
-
-    _, _, residuals = problem.model.get_nonlinear_vectors()
-    residuals = filter_residuals(residuals)
-
-    problem.write_outputs()
-
-    assert problem.get_val("data:mission:operational:TOW", units="kg") == pytest.approx(
-        5670.0, rel=5e-2
-    )
-    assert problem.get_val("data:mission:operational:fuel", units="kg") == pytest.approx(
-        1163.00, rel=5e-2
-    )
-
-    assert problem.get_val(
-        "data:environmental_impact:operational:fuel_emissions", units="kg"
-    ) == pytest.approx(4271.75, rel=1e-2)
