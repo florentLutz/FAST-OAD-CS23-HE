bb4aea0bf1e03c513f0e638db6a1e642a0d766ef
johnathanasd <73662010+johnathanasd@users.noreply.github.com>
2026-02-11 09:35:32 +0100

    af file moved to data folder and airfoil update for the h2

--- integration_tests/dhc_6_twin_otter/test_sizing_dhc6_twin_otter.py
+++ integration_tests/dhc_6_twin_otter/test_sizing_dhc6_twin_otter.py
@@ -103,11 +103,11 @@ def test_sizing_twin_otter_pemfc_h2():
 
     problem.write_outputs()
 
-    assert problem.get_val("data:weight:aircraft:MTOW", units="kg") == pytest.approx(5588, rel=1e-2)
+    assert problem.get_val("data:weight:aircraft:MTOW", units="kg") == pytest.approx(5564, rel=1e-2)
     assert problem.get_val("data:weight:aircraft:MLW", units="kg") == pytest.approx(
-        5553.2, rel=1e-2
+        5530.5, rel=1e-2
     )
     assert problem.get_val("data:weight:aircraft:OWE", units="kg") == pytest.approx(
-        4384.7, rel=1e-2
+        4362.3, rel=1e-2
     )
-    assert problem.get_val("data:mission:sizing:fuel", units="kg") == pytest.approx(114.7, rel=1e-2)
+    assert problem.get_val("data:mission:sizing:fuel", units="kg") == pytest.approx(113.4, rel=1e-2)
