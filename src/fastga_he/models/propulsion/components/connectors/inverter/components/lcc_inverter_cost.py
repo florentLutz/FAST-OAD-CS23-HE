909d5f4874421a5cb16e7f8f6856bcf3937a9b5a
johnathanasd <93048.87051.ko@gmail.com>
2026-05-18 14:50:58 +0200

    copyright update

--- src/fastga_he/models/propulsion/components/connectors/inverter/components/lcc_inverter_cost.py
+++ src/fastga_he/models/propulsion/components/connectors/inverter/components/lcc_inverter_cost.py
@@ -1,6 +1,6 @@
 # This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
 # Electric Aircraft.
-# Copyright (C) 2025 ISAE-SUPAERO
+# Copyright (C) 2026 ISAE-SUPAERO
 
 import openmdao.api as om
 
@@ -42,6 +42,7 @@ class LCCInverterCost(om.ExplicitComponent):
             desc="Unit purchase cost of the inverter",
         )
 
+    def setup_partials(self):
         self.declare_partials(of="*", wrt="*", val=575.0)
 
     def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
