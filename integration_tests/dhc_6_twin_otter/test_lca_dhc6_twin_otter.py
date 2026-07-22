68d1b051d65bf6fbdab7a61e589491f75da565af
Florent <66668699+florentLutz@users.noreply.github.com>
2026-07-22 16:36:28 +0200

    Added custom LCIA for EF and ReCiPe
    Added the impact of hydrogen release for EF no LT and ReCiPe. Note that it hasn't been implemented for Impact World + currenly.

--- integration_tests/dhc_6_twin_otter/test_lca_dhc6_twin_otter.py
+++ integration_tests/dhc_6_twin_otter/test_lca_dhc6_twin_otter.py
@@ -36,3 +36,26 @@ def test_lca_twin_otter_pemfc_h2():
     assert problem.get_val("data:environmental_impact:single_score") == pytest.approx(
         2.557e-05, rel=1e-3
     )
+
+
+def test_lca_twin_otter_pemfc_h2_recipe():
+    """Test the overall aircraft design process with wing positioning."""
+    logging.basicConfig(level=logging.WARNING)
+    logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
+    logging.getLogger("fastoad.openmdao.variables.variable").disabled = True
+
+    # Define used files depending on options
+    xml_file_name = "lca_pemfc_h2_gas.xml"
+    process_file_name = "lca_pemfc_h2_twin_otter_recipe.yml"
+
+    configurator = oad.FASTOADProblemConfigurator(DATA_FOLDER_PATH / process_file_name)
+    problem = configurator.get_problem()
+
+    # Create inputs
+    ref_inputs = DATA_FOLDER_PATH / xml_file_name
+
+    problem.write_needed_inputs(ref_inputs)
+    problem.read_inputs()
+    problem.setup()
+    problem.run_model()
+    problem.write_outputs()
