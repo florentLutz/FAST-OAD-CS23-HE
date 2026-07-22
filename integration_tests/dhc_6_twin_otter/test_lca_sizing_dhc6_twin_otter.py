616068c69a378df5a1550687911f2c404d96564b
Florent <66668699+florentLutz@users.noreply.github.com>
2026-07-22 09:17:28 +0200

    Addition of LCA test case.
    Addition of LCA test case in a separate file to not clash with other branches

---
+++ integration_tests/dhc_6_twin_otter/test_lca_sizing_dhc6_twin_otter.py
@@ -0,0 +1,34 @@
+#  This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
+#  Electric Aircraft.
+#  Copyright (C) 2026 ISAE-SUPAERO
+
+import logging
+import pathlib
+
+import fastoad.api as oad
+
+DATA_FOLDER_PATH = pathlib.Path(__file__).parent / "data"
+RESULTS_FOLDER_PATH = pathlib.Path(__file__).parent / "results"
+
+
+def test_lca_twin_otter_pemfc_h2():
+    """Test the overall aircraft design process with wing positioning."""
+    logging.basicConfig(level=logging.WARNING)
+    logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
+    logging.getLogger("fastoad.openmdao.variables.variable").disabled = True
+
+    # Define used files depending on options
+    xml_file_name = "lca_pemfc_h2_gas.xml"
+    process_file_name = "lca_pemfc_h2_twin_otter.yml"
+
+    configurator = oad.FASTOADProblemConfigurator(DATA_FOLDER_PATH / process_file_name)
+    problem = configurator.get_problem()
+
+    # Create inputs
+    ref_inputs = RESULTS_FOLDER_PATH / xml_file_name
+
+    problem.write_needed_inputs(ref_inputs)
+    problem.read_inputs()
+    problem.setup()
+    problem.run_model()
+    problem.write_outputs()
