fdfbfe228b467f2c5e9642e9c4d52aad2f40bd7a
johnathanasd <93048.87051.ko@gmail.com>
2026-09-03 11:59:19 +0200

    Update revenue per rpk with no loan financing

--- integration_tests/dhc_6_twin_otter/test_lcc_dhc6_twin_otter.py
+++ integration_tests/dhc_6_twin_otter/test_lcc_dhc6_twin_otter.py
@@ -38,7 +38,7 @@ def test_lcc_twin_otter_for_easn():
     problem.write_outputs()
 
     assert problem.get_val("data:cost:operation:revenue_per_rpk", units="USD/km") == pytest.approx(
-        0.863, rel=1e-2
+        0.739, rel=1e-2
     )
 
 
@@ -65,7 +65,7 @@ def test_lcc_pemfc_h2_twin_otter_for_easn():
     problem.write_outputs()
 
     assert problem.get_val("data:cost:operation:revenue_per_rpk", units="USD/km") == pytest.approx(
-        1.512, rel=1e-2
+        1.278, rel=1e-2
     )
 
 
@@ -92,175 +92,5 @@ def test_lcc_pemfc_h2_twin_otter_hybrid_for_easn():
     problem.write_outputs()
 
     assert problem.get_val("data:cost:operation:revenue_per_rpk", units="USD/km") == pytest.approx(
-        0.803, rel=1e-2
+        0.686, rel=1e-2
     )
-
-
-def test_lca_pemfc_h2_twin_otter_for_easn_technology_sensitivity():
-    logging.basicConfig(level=logging.WARNING)
-    logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
-    logging.getLogger("fastoad.openmdao.variables.variable").disabled = True
-
-    # Define used files depending on options
-    xml_file_name = "input_h2_pemfc_twin_otter_retrofit_for_easn.xml"
-    process_file_name = "oad_lca_pemfc_h2_twin_otter_for_easn.yml"
-
-    ref_inputs = DATA_FOLDER_PATH / xml_file_name
-
-    configurator = oad.FASTOADProblemConfigurator(DATA_FOLDER_PATH / process_file_name)
-    problem = configurator.get_problem()
-
-    # Create inputs, setup, and complete with missing LCA assumptions
-    datafile = oad.DataFile(ref_inputs)
-    # Great circle distance between Calgary and Toulouse
-    datafile.append(
-        oad.Variable("data:environmental_impact:delivery:distance", val=8000.0, units="km")
-    )
-    # Standard assumption
-    datafile.append(
-        oad.Variable("data:environmental_impact:line_test:duration", val=10.0, units="h")
-    )
-    # According to TCDS, propeller is aluminium
-    datafile.append(
-        oad.Variable("data:propulsion:he_power_train:propeller:propeller_1:material", val=0.0)
-    )
-    datafile.append(
-        oad.Variable("data:propulsion:he_power_train:propeller:propeller_2:material", val=0.0)
-    )
-    datafile.save()
-
-    problem.model_options["*propeller_*"] = {"mass_as_input": True}
-
-    problem.write_needed_inputs(ref_inputs)
-    problem.read_inputs()
-    problem.setup()
-
-    # Based on the wing box lifespan, other structural parts last longer
-    problem.set_val("data:TLAR:max_airframe_hours", val=33000, units="h")
-    # Based on Viking Air assumptions
-    problem.set_val("data:TLAR:flight_hours_per_year", val=1200.0, units="h")
-    problem.set_val("data:environmental_impact:buy_to_fly:metallic", val=7.5)
-
-    # Low technology factor = more advanced
-    for k_factor_technology in np.linspace(0.0, 1.0, 11):
-        k_factor_density = np.interp(k_factor_technology, [0.0, 1.0], [0.6712, 1.0])
-        k_factor_safety = np.interp(k_factor_technology, [0.0, 1.0], [1.0, 2.25])
-        # Could extend to TMS performance as well
-        problem.set_val(
-            "settings:propulsion:he_power_train:PEMFC_stack:pemfc_stack_1:k_mass",
-            val=k_factor_density,
-        )
-        problem.set_val(
-            "data:propulsion:he_power_train:gaseous_hydrogen_tank:gaseous_hydrogen_tank_1:safety_factor",
-            val=k_factor_safety,
-        )
-        # Run the problem
-        problem.run_model()
-        problem.output_file_path = (
-            RESULTS_FOLDER_PATH
-            / "technology_sensitivity"
-            / (str(round(k_factor_technology, 2)).replace(".", "_") + "_k_factor_technology.xml")
-        )
-        problem.write_outputs()
-
-
-def test_lca_doe_pemfc_h2_twin_otter_hybrid_for_easn():
-    logging.basicConfig(level=logging.WARNING)
-    logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
-    logging.getLogger("fastoad.openmdao.variables.variable").disabled = True
-
-    # Define used files depending on options
-    xml_file_name = "input_retrofit_pemfc_h2_twin_otter_hybrid_optim_for_easn.xml"
-    process_file_name = "retrofit_pemfc_h2_twin_otter_hybrid_optim_for_easn.yml"
-
-    ref_inputs = DATA_FOLDER_PATH / xml_file_name
-    configurator = oad.FASTOADProblemConfigurator(DATA_FOLDER_PATH / process_file_name)
-    problem = configurator.get_problem()
-
-    problem.model_options["*propeller_*"] = {"mass_as_input": True}
-    problem.model_options["*pemfc_stack_*"] = {"mass_from_specific_power": True}
-
-    problem.write_needed_inputs(ref_inputs)
-    problem.read_inputs()
-    problem.setup()
-
-    # Any lower than 60% gives unrealistic values for turboshaft.
-    for power_split in [60, 65, 70, 75, 80]:
-        problem.set_val(
-            "data:propulsion:he_power_train:planetary_gear:planetary_gear_1:power_split",
-            val=power_split,
-            units="percent",
-        )
-        problem.set_val(
-            "data:propulsion:he_power_train:planetary_gear:planetary_gear_2:power_split",
-            val=power_split,
-            units="percent",
-        )
-        # Run the problem
-        problem.output_file_path = (
-            RESULTS_FOLDER_PATH / "percent_split_doe" / (str(power_split) + ".xml")
-        )
-        problem.run_model()
-        problem.write_outputs()
-
-
-def test_lca_pemfc_h2_twin_otter_optim_for_easn():
-    logging.basicConfig(level=logging.WARNING)
-    logging.getLogger("fastoad.module_management._bundle_loader").disabled = True
-    logging.getLogger("fastoad.openmdao.variables.variable").disabled = True
-
-    # Define used files depending on options
-    xml_file_name = "input_retrofit_pemfc_h2_twin_otter_hybrid_optim_for_easn.xml"
-    process_file_name = "retrofit_pemfc_h2_twin_otter_hybrid_optim_for_easn.yml"
-
-    ref_inputs = DATA_FOLDER_PATH / xml_file_name
-    configurator = oad.FASTOADProblemConfigurator(DATA_FOLDER_PATH / process_file_name)
-    problem = configurator.get_problem()
-
-    problem.model_options["*propeller_*"] = {"mass_as_input": True}
-    problem.model_options["*pemfc_stack_*"] = {"mass_from_specific_power": True}
-
-    problem.write_needed_inputs(ref_inputs)
-    problem.read_inputs()
-    problem.model.approx_totals()
-
-    problem.model.add_design_var(
-        name="data:propulsion:he_power_train:planetary_gear:planetary_gear_1:power_split",
-        units="percent",
-        lower=60.0,
-        upper=90.0,
-    )
-    problem.model.add_design_var(
-        name="data:propulsion:he_power_train:planetary_gear:planetary_gear_2:power_split",
-        units="percent",
-        lower=60.0,
-        upper=90.0,
-    )
-
-    problem.model.add_objective(name="data:environmental_impact:single_score", scaler=1e7)
-
-    recorder = om.SqliteRecorder("driver_cases_for_easn.sql")
-    problem.driver.add_recorder(recorder)
-
-    problem.driver.recording_options["record_desvars"] = True
-    problem.driver.recording_options["record_objectives"] = True
-
-    problem.model_options["*propeller_*"] = {"mass_as_input": True}
-    problem.model_options["*pemfc_stack_*"] = {"mass_from_specific_power": True}
-
-    problem.setup()
-
-    problem.set_val(
-        "data:propulsion:he_power_train:planetary_gear:planetary_gear_1:power_split",
-        val=60,
-        units="percent",
-    )
-    problem.set_val(
-        "data:propulsion:he_power_train:planetary_gear:planetary_gear_2:power_split",
-        val=60,
-        units="percent",
-    )
-
-    problem.run_driver()
-    problem.output_file_path = RESULTS_FOLDER_PATH / "optim_power_split_with_lca.xml"
-    problem.write_outputs()
